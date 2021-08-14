b3154033520267699aa5acfc0e27302be5a8a80e
Ludvig Lundgren <hello@ludviglundgren.se>
2021-08-14 02:32:35 +0200

    build: move static assets

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 
 import {RecoilRoot} from 'recoil';
 import {APP} from "./domain/interfaces";
@@ -21,8 +20,3 @@ ReactDOM.render(
     </React.StrictMode>,
     document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(console.log);
