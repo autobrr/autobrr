fa6b78223806a99a1e00ffba55bfe7984bc20b8c
Ludvig Lundgren <hello@ludviglundgren.se>
2021-12-27 22:03:55 +0100

    refactor: simplify tag split and trim

--- internal/domain/release.go
+++ internal/domain/release.go
@@ -560,7 +560,7 @@ func (r *Release) MapVars(varMap map[string]string) error {
 	}
 
 	if tags, err := getStringMapValue(varMap, "tags"); err == nil {
-		tagArr := splitAndTrimString(tags, ",")
+		tagArr := strings.Split(strings.TrimSpace(tags), ",")
 		r.Tags = tagArr
 	}
 
@@ -933,15 +933,6 @@ func findLastInt(input string, pattern string) (int, error) {
 	}
 
 	return 0, nil
-}
-
-func splitAndTrimString(str string, separator string) []string {
-	arr := strings.Split(str, separator)
-	for i := range arr {
-		arr[i] = strings.TrimSpace(arr[i])
-	}
-
-	return arr
 }
 
 //func Splitter(s string, splits string) []string {
