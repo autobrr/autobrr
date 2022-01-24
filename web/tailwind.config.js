170d370aa5fe14bb3d4699fc9395a3a4e6c97606
stacksmash76 <stacksmash76@protonmail.com>
2022-01-24 21:10:35 +0100

    Fixed previous mistake regarding tailwind.config.js purge key, since we're still using old postcss7 from October last year

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,23 +1,25 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-    content: [
-        './src/**/*.{tsx,ts,html,css}',
-    ],
-    safelist: [
-        'col-span-1',
-        'col-span-2',
-        'col-span-3',
-        'col-span-4',
-        'col-span-5',
-        'col-span-6',
-        'col-span-7',
-        'col-span-8',
-        'col-span-9',
-        'col-span-10',
-        'col-span-11',
-        'col-span-12',
-    ],
+    purge: {
+        content: [
+            './src/**/*.{tsx,ts,html,css}',
+        ],
+        safelist: [
+            'col-span-1',
+            'col-span-2',
+            'col-span-3',
+            'col-span-4',
+            'col-span-5',
+            'col-span-6',
+            'col-span-7',
+            'col-span-8',
+            'col-span-9',
+            'col-span-10',
+            'col-span-11',
+            'col-span-12',
+        ],
+    },
     darkMode: 'class', // or 'media' or 'class'
     theme: {
         extend: {
