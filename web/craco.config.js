fb4a8682690ced2a3117c379609327183e7ab3bc
Raghu Saxena <poiasdpoiasd@live.com>
2022-01-25 16:14:41 +0800

    Update most web dependencies

--- web/craco.config.js
+++ web/craco.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     style: {
-        postcss: {
+        postcssOptions: {
             plugins: [
                 require('tailwindcss'),
                 require('autoprefixer'),
