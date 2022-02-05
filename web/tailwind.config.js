7f136c1828ada335bda98a8605e06fcee49fa97d
Ludvig Lundgren <hello@ludviglundgren.se>
2022-02-05 12:43:20 +0100

    chore: update deps and remove craco

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,32 +1,16 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-    purge: {
-        content: [
-            './src/**/*.{tsx,ts,html,css}',
-        ],
-        safelist: [
-            'col-span-1',
-            'col-span-2',
-            'col-span-3',
-            'col-span-4',
-            'col-span-5',
-            'col-span-6',
-            'col-span-7',
-            'col-span-8',
-            'col-span-9',
-            'col-span-10',
-            'col-span-11',
-            'col-span-12',
-        ],
-    },
+    content: [
+        './src/**/*.{tsx,ts,html,css}',
+    ],
+    // purge: false,
     darkMode: 'class', // or 'media' or 'class'
     theme: {
         extend: {
             colors: {
-                gray: colors.gray,
-                teal: colors.teal,
-            }
+                gray: colors.neutral,
+            },
         },
     },
     variants: {
