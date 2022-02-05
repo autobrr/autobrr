787cf183cf2e70b0269d73639f7a1c841510ff8e
Ludvig Lundgren <hello@ludviglundgren.se>
2022-02-05 12:55:50 +0100

    fix: add back safelist

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -4,6 +4,20 @@ module.exports = {
     content: [
         './src/**/*.{tsx,ts,html,css}',
     ],
+    safelist: [
+        'col-span-1',
+        'col-span-2',
+        'col-span-3',
+        'col-span-4',
+        'col-span-5',
+        'col-span-6',
+        'col-span-7',
+        'col-span-8',
+        'col-span-9',
+        'col-span-10',
+        'col-span-11',
+        'col-span-12',
+    ],
     // purge: false,
     darkMode: 'class', // or 'media' or 'class'
     theme: {
