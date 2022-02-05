8c9f9495ba4ad6b8cfc95fb3b2a041fb0c4a96f3
Ludvig Lundgren <hello@ludviglundgren.se>
2022-02-05 15:20:54 +0100

    feat: web add more types (#114)

--- web/src/types/Indexer.d.ts
+++ web/src/types/Indexer.d.ts
@@ -1,42 +1,64 @@
 interface Indexer {
-    id: number;
-    name: string;
-    identifier: string;
-    enabled: boolean;
-    settings: object | any;
+  id: number;
+  name: string;
+  identifier: string;
+  enabled: boolean;
+  type?: string;
+  settings: Array<IndexerSetting>;
 }
 
-interface IndexerSchema {
-    name: string;
-    identifier: string;
-    description: string;
-    language: string;
-    privacy: string;
-    protocol: string;
-    urls: string[];
-    settings: IndexerSchemaSettings[];
-    irc: IndexerSchemaIRC;
+interface IndexerDefinition {
+  id?: number;
+  name: string;
+  identifier: string;
+  enabled?: boolean;
+  description: string;
+  language: string;
+  privacy: string;
+  protocol: string;
+  urls: string[];
+  supports: string[];
+  settings: IndexerSetting[];
+  irc: IndexerIRC;
+  parse: IndexerParse;
 }
 
-interface IndexerSchemaSettings {
-    name: string;
-    type: string;
-    required: boolean;
-    label: string;
-    help: string;
-    description: string;
-    default: string;
+interface IndexerSetting {
+  name: string;
+  required?: boolean;
+  type: string;
+  value?: string;
+  label: string;
+  default?: string;
+  description?: string;
+  help?: string;
+  regex?: string;
 }
 
-interface IndexerSchemaIRC {
-    network: string;
-    server: string;
-    port: number;
-    tls: boolean;
-    nickserv: boolean;
-    announcers: string[];
-    channels: string[];
-    invite: string[];
-    invite_command: string;
-    settings: IndexerSchemaSettings[];
+interface IndexerIRC {
+  network: string;
+  server: string;
+  port: number;
+  tls: boolean;
+  nickserv: boolean;
+  channels: string[];
+  announcers: string[];
+  settings: IndexerSetting[];
+}
+
+interface IndexerParse {
+  type: string;
+  lines: IndexerParseLines[];
+  match: IndexerParseMatch;
+}
+
+interface IndexerParseLines {
+  test: string[];
+  pattern: string;
+  vars: string[];
+}
+
+interface IndexerParseMatch {
+  torrentUrl: string;
+  encode: string[];
 }
