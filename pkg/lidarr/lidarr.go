178ebeb48f6ed5adcd169c8a9208deb5d90be3da
Ludvig Lundgren <hello@ludviglundgren.se>
2022-04-18 15:28:03 +0200

    feat: lidarr improve logging and errors

--- pkg/lidarr/lidarr.go
+++ pkg/lidarr/lidarr.go
@@ -2,7 +2,7 @@ package lidarr
 
 import (
 	"encoding/json"
-	"io"
+	"errors"
 	"net/http"
 	"strings"
 	"time"
@@ -67,59 +67,43 @@ type SystemStatusResponse struct {
 }
 
 func (c *client) Test() (*SystemStatusResponse, error) {
-	res, err := c.get("system/status")
+	status, res, err := c.get("system/status")
 	if err != nil {
 		log.Error().Stack().Err(err).Msg("lidarr client get error")
 		return nil, err
 	}
 
-	defer res.Body.Close()
-
-	body, err := io.ReadAll(res.Body)
-	if err != nil {
-		log.Error().Stack().Err(err).Msg("lidarr client error reading body")
-		return nil, err
+	if status == http.StatusUnauthorized {
+		return nil, errors.New("unauthorized: bad credentials")
 	}
 
+	log.Trace().Msgf("lidarr system/status response status: %v body: %v", status, string(res))
+
 	response := SystemStatusResponse{}
-	err = json.Unmarshal(body, &response)
+	err = json.Unmarshal(res, &response)
 	if err != nil {
 		log.Error().Stack().Err(err).Msg("lidarr client error json unmarshal")
 		return nil, err
 	}
-
-	log.Trace().Msgf("lidarr system/status response: %+v", response)
 
 	return &response, nil
 }
 
 func (c *client) Push(release Release) ([]string, error) {
-	res, err := c.post("release/push", release)
+	status, res, err := c.postBody("release/push", release)
 	if err != nil {
 		log.Error().Stack().Err(err).Msg("lidarr client post error")
 		return nil, err
 	}
 
-	if res == nil {
-		return nil, err
-	}
-
-	defer res.Body.Close()
-
-	body, err := io.ReadAll(res.Body)
-	if err != nil {
-		log.Error().Stack().Err(err).Msg("lidarr client error reading body")
-		return nil, err
-	}
+	log.Trace().Msgf("lidarr release/push response status: %v body: %v", status, string(res))
 
 	pushResponse := PushResponse{}
-	err = json.Unmarshal(body, &pushResponse)
+	err = json.Unmarshal(res, &pushResponse)
 	if err != nil {
 		log.Error().Stack().Err(err).Msg("lidarr client error json unmarshal")
 		return nil, err
 	}
-
-	log.Trace().Msgf("lidarr release/push response body: %+v", string(body))
 
 	// log and return if rejected
 	if pushResponse.Rejected {
