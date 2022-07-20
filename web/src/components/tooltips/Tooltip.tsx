5a1209bf20dd0d9b2b5c815206e18a460bde404d
anonymous <anonymous>
2022-07-20 23:10:25 +0200

    fix: tooltip text wrapping

--- web/src/components/tooltips/Tooltip.tsx
+++ web/src/components/tooltips/Tooltip.tsx
@@ -6,9 +6,10 @@ export const Tooltip = ({ children, button } : {
   return (
     <div className="relative flex flex-col items-center group">
       {button}
-      <div className="absolute bottom-0 flex flex-col items-center hidden mb-6 group-hover:flex">
-        <span className="relative z-40 p-2 text-xs leading-none text-white whitespace-no-wrap bg-gray-600 shadow-lg rounded-md">{children}</span>
-        <div className="w-3 h-3 -mt-2 rotate-45 bg-gray-600"></div>
+      <div className="absolute bottom-0 flex-col items-center hidden mb-6 group-hover:flex">
+        <span className="z-40 p-2 text-xs leading-none text-white whitespace-no-wrap bg-gray-600 shadow-lg rounded-md">
+          {children}
+        </span>
       </div>
     </div>
   );
