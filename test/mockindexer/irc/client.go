0f2ce26ba266822909d4bd80b2e0dc1ed5e40f31
Viktor Elofsson <viktor@viktorelofsson.se>
2022-12-03 16:55:36 +0100

    feat(tools): Add a simple MockIndexer helper for IRC announcing and indexing (#555)
    * Add a simple SelfIndexer for IRC announcing and indexing
    
    * Rename to MockIndexer
    
    * fix: close file after reading

---
+++ test/mockindexer/irc/client.go
@@ -0,0 +1,59 @@
+package irc
+
+import (
+	"bufio"
+	"log"
+	"net"
+	"strings"
+)
+
+type Client struct {
+	conn   net.Conn
+	writer chan string
+
+	botName     string
+	channelName string
+	nick        string
+	user        string
+
+	handler func(c *Client, cmd []string)
+}
+
+type ClientHandler interface {
+	Handle(c Client, cmd []string)
+}
+
+func NewClient(conn net.Conn, botName, channelName string) *Client {
+	client := &Client{
+		botName:     botName,
+		channelName: channelName,
+		conn:        conn,
+		writer:      make(chan string),
+	}
+
+	client.handler = RegistrationHandler
+
+	go client.readerLoop()
+	go client.writerLoop()
+
+	return client
+}
+
+func (c *Client) readerLoop() {
+	scanner := bufio.NewScanner(c.conn)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		cmd := strings.Split(line, " ")
+
+		log.Printf("%s", scanner.Text())
+
+		c.handler(c, cmd)
+	}
+}
+
+func (c *Client) writerLoop() {
+	for cmd := range c.writer {
+		c.conn.Write([]byte(cmd + "\r\n"))
+	}
+}
