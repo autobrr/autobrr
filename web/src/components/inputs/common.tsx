20841d279ec5c840f0432c36cb9e0dd8f7bdbd28
martylukyy <35452459+martylukyy@users.noreply.github.com>
2023-01-01 23:18:16 +0100

    put dark last for uniformity

--- web/src/components/inputs/common.tsx
+++ web/src/components/inputs/common.tsx
@@ -37,7 +37,7 @@ const CheckboxField = ({
         type="checkbox" 
         className={classNames(
           "focus:ring-blue-500 h-4 w-4 text-blue-600 border-gray-300 rounded", 
-          disabled ? "dark:bg-gray-700 dark:border-gray-700 bg-gray-200" : ""
+          disabled ? "bg-gray-200 dark:bg-gray-700 dark:border-gray-700" : ""
         )}
         disabled={disabled}
       />
