00fe2f2c7ea37778f122c90b5ef434f1da1c20e3
martylukyy <35452459+martylukyy@users.noreply.github.com>
2023-01-01 23:00:06 +0100

    enhancement(web): gray color for disabled checkboxes

--- web/src/components/inputs/common.tsx
+++ web/src/components/inputs/common.tsx
@@ -35,7 +35,10 @@ const CheckboxField = ({
         id={name}
         name={name}
         type="checkbox" 
-        className={classNames("focus:ring-blue-500 h-4 w-4 text-blue-600 border-gray-300 rounded", disabled ? "bg-gray-200" : "")}
+        className={classNames(
+          "focus:ring-blue-500 h-4 w-4 text-blue-600 border-gray-300 rounded", 
+          disabled ? "bg-gray-700 border-gray-700" : "bg-gray-200"
+        )}
         disabled={disabled}
       />
     </div>
