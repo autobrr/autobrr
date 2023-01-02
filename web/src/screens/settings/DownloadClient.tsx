b8ced5d59a51c8873d873a649e33bff0b3832a96
martylukyy <35452459+martylukyy@users.noreply.github.com>
2023-01-02 22:59:56 +0000

    chore(web): fix indentation (#607)

--- web/src/screens/settings/DownloadClient.tsx
+++ web/src/screens/settings/DownloadClient.tsx
@@ -43,32 +43,32 @@ function DownloadClientSettingsListItem({ client, idx }: DLSettingsItemProps) {
           isOpen={updateClientIsOpen}
           toggle={toggleUpdateClient}
         />
-          <div className="col-span-3 sm:col-span-2 px-4 sm:px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-            <Switch
-              checked={client.enabled}
-              onChange={onToggleMutation}
+        <div className="col-span-3 sm:col-span-2 px-4 sm:px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+          <Switch
+            checked={client.enabled}
+            onChange={onToggleMutation}
+            className={classNames(
+              client.enabled ? "bg-blue-500" : "bg-gray-200 dark:bg-gray-600",
+              "relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+            )}
+          >
+            <span className="sr-only">Use setting</span>
+            <span
+              aria-hidden="true"
               className={classNames(
-                client.enabled ? "bg-blue-500" : "bg-gray-200 dark:bg-gray-600",
-                "relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                client.enabled ? "translate-x-5" : "translate-x-0",
+                "inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200"
               )}
-            >
-              <span className="sr-only">Use setting</span>
-              <span
-                aria-hidden="true"
-                className={classNames(
-                  client.enabled ? "translate-x-5" : "translate-x-0",
-                  "inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200"
-                )}
-              />
-            </Switch>
-          </div>
-          <div className="col-span-7 sm:col-span-3 px-1 sm:px-0 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-white truncate" title={client.name}>{client.name}</div>
-          <div className="hidden sm:block col-span-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-400 truncate" title={client.host}>{client.host}</div>
-          <div className="hidden sm:block col-span-2 whitespace-nowrap text-sm text-gray-500 dark:text-gray-400">{DownloadClientTypeNameMap[client.type]}</div>
-          <div className="col-span-1 whitespace-nowrap text-center text-sm font-medium">
-            <span className="text-blue-600 dark:text-gray-300 hover:text-blue-900 cursor-pointer" onClick={toggleUpdateClient}>
-              Edit
-            </span>
+            />
+          </Switch>
+        </div>
+        <div className="col-span-7 sm:col-span-3 px-1 sm:px-0 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-white truncate" title={client.name}>{client.name}</div>
+        <div className="hidden sm:block col-span-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-400 truncate" title={client.host}>{client.host}</div>
+        <div className="hidden sm:block col-span-2 whitespace-nowrap text-sm text-gray-500 dark:text-gray-400">{DownloadClientTypeNameMap[client.type]}</div>
+        <div className="col-span-1 whitespace-nowrap text-center text-sm font-medium">
+          <span className="text-blue-600 dark:text-gray-300 hover:text-blue-900 cursor-pointer" onClick={toggleUpdateClient}>
+            Edit
+          </span>
         </div>
       </div>
     </li>
