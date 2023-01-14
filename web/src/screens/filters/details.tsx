d3a7dfeb536effd2b942d2ffe929226e329e71ba
soup <soup@r4tio.dev>
2023-01-14 15:22:08 +0100

    removed max value by mistake

--- web/src/screens/filters/details.tsx
+++ web/src/screens/filters/details.tsx
@@ -435,7 +435,7 @@ export function Music({ values }: AdvancedProps) {
         </div>
 
         <div className="mt-6 grid grid-cols-12 gap-6">
-          <NumberField name="log_score" label="Log score" placeholder="eg. 100" min={0} required={true} disabled={values.perfect_flac} />
+          <NumberField name="log_score" label="Log score" placeholder="eg. 100" min={0} max={100} required={true} disabled={values.perfect_flac} />
         </div>
 
       </div>
