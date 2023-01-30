8a5c261e32c5bf4aea45d047559fc74d56d0c5a9
martylukyy <35452459+martylukyy@users.noreply.github.com>
2023-01-30 10:42:33 +0100

    replaced info icon with a more rudimentary one

--- web/src/components/tooltips/CustomTooltip.tsx
+++ web/src/components/tooltips/CustomTooltip.tsx
@@ -1,5 +1,4 @@
 import { Tooltip } from "react-tooltip";
-import { InformationCircleIcon } from "@heroicons/react/24/solid";
 import "./CustomTooltip.css";
 
 
@@ -16,7 +15,7 @@ export const CustomTooltip = ({
   clickable
 }: CustomTooltipProps) => (
   <div>
-    <InformationCircleIcon className="ml-1 h-4 w-4 text-gray-300" aria-hidden="true" />
+    <svg className="ml-1 w-4 h-4" viewBox="0 0 72 72"><path d="M32 2C15.432 2 2 15.432 2 32s13.432 30 30 30s30-13.432 30-30S48.568 2 32 2m5 49.75H27v-24h10v24m-5-29.5a5 5 0 1 1 0-10a5 5 0 0 1 0 10" fill="currentcolor"/></svg>
     <Tooltip style= {{ maxWidth: "350px", fontSize: "12px", textTransform: "none", fontWeight: "normal", borderRadius: "0.375rem", backgroundColor: "#34343A", color: "#fff", opacity: "1" }} delayShow={100} delayHide={150} place="top" anchorId={anchorId} data-html={true} clickable={clickable}>
       {children}
     </Tooltip>
