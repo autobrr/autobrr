d1d3f0bb9785526d7f08932d2e94248f49225e73
ze0s <ze0s@riseup.net>
2023-02-12 03:46:37 +0100

    feat(logs): make logs settings dropdown

--- web/src/screens/Logs.tsx
+++ web/src/screens/Logs.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { Fragment, useEffect, useRef, useState } from "react";
 import { ExclamationTriangleIcon } from "@heroicons/react/24/solid";
 import format from "date-fns/format";
 import { DebounceInput } from "react-debounce-input";
@@ -7,9 +7,19 @@ import { Checkbox } from "../components/Checkbox";
 import { baseUrl, classNames, simplifyDate } from "../utils";
 import { SettingsContext } from "../utils/Context";
 import { EmptySimple } from "../components/emptystates";
-import { DocumentArrowDownIcon } from "@heroicons/react/24/outline";
-import { useQuery } from "react-query";
+import {
+  Cog6ToothIcon,
+  DocumentArrowDownIcon,
+  EllipsisHorizontalIcon,
+  PencilSquareIcon, TrashIcon
+} from "@heroicons/react/24/outline";
+import { useMutation, useQuery, useQueryClient } from "react-query";
 import { Link } from "react-router-dom";
+import { useToggle } from "../hooks/hooks";
+import { toast } from "react-hot-toast";
+import Toast from "../components/notifications/Toast";
+import { Menu, Transition } from "@headlessui/react";
+import { DeleteModal } from "../components/modals";
 
 type LogEvent = {
   time: string;
@@ -17,13 +27,14 @@ type LogEvent = {
   message: string;
 };
 
-type LogLevel = "TRACE" | "DEBUG" | "INFO" | "ERROR";
+type LogLevel = "TRACE" | "DEBUG" | "INFO" | "ERROR" | "WARN";
 
 const LogColors: Record<LogLevel, string> = {
   "TRACE": "text-purple-300",
   "DEBUG": "text-yellow-500",
   "INFO": "text-green-500",
-  "ERROR": "text-red-500"
+  "ERROR": "text-red-500",
+  "WARN": "text-yellow-500"
 };
 
 export const Logs = () => {
@@ -81,35 +92,38 @@ export const Logs = () => {
       <header className="pt-10 pb-5">
         <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
           <h1 className="text-3xl font-bold text-black dark:text-white">Logs</h1>
-          <div className="flex justify-center mt-1">
-            <ExclamationTriangleIcon
-              className="h-5 w-5 text-yellow-400"
-              aria-hidden="true"
-            />
-            <p className="ml-2 text-sm text-black dark:text-gray-400">This page shows only new logs, i.e. no history.</p>
-          </div>
         </div>
       </header>
+
+
       <div className="max-w-screen-xl mx-auto pb-12 px-2 sm:px-4 lg:px-8">
-        <div
-          className="bg-white dark:bg-gray-800 rounded-lg shadow-lg px-2 sm:px-4 pt-3 sm:pt-4"
-        >
-          <DebounceInput
-            minLength={2}
-            debounceTimeout={200}
-            onChange={(event) => setSearchFilter(event.target.value.toLowerCase().trim())}
-            id="filter"
-            type="text"
-            autoComplete="off"
-            className={classNames(
-              "focus:ring-blue-500 dark:focus:ring-blue-500 focus:border-blue-500 dark:focus:border-blue-500 border-gray-300 dark:border-gray-700",
-              "block w-full dark:bg-gray-800 shadow-sm dark:text-gray-100 sm:text-sm rounded-md"
-            )}
-            placeholder="Enter a string to filter logs by..."
+        <div className="flex justify-center py-4">
+          <ExclamationTriangleIcon
+            className="h-5 w-5 text-yellow-400"
+            aria-hidden="true"
           />
-          <div
-            className="mt-2 overflow-y-auto p-2 rounded-lg h-[60vh] min-w-full bg-gray-100 dark:bg-gray-900 overflow-auto"
-          >
+          <p className="ml-2 text-sm text-black dark:text-gray-400">This page shows only new logs, i.e. no history.</p>
+        </div>
+        <div className="bg-white dark:bg-gray-800 rounded-lg shadow-lg px-2 sm:px-4 pt-3 sm:pt-4 pb-3 sm:pb-4">
+          <div className="flex relative mb-3">
+            <DebounceInput
+              minLength={2}
+              debounceTimeout={200}
+              onChange={(event) => setSearchFilter(event.target.value.toLowerCase().trim())}
+              id="filter"
+              type="text"
+              autoComplete="off"
+              className={classNames(
+                "focus:ring-blue-500 dark:focus:ring-blue-500 focus:border-blue-500 dark:focus:border-blue-500 border-gray-300 dark:border-gray-700",
+                "block w-full dark:bg-gray-900 shadow-sm dark:text-gray-100 sm:text-sm rounded-md"
+              )}
+              placeholder="Enter a string to filter logs by..."
+            />
+
+            <LogsDropdown />
+          </div>
+
+          <div className="overflow-y-auto px-2 rounded-lg h-[60vh] min-w-full bg-gray-100 dark:bg-gray-900 overflow-auto">
             {filteredLogs.map((entry, idx) => (
               <div
                 key={idx}
@@ -140,29 +154,13 @@ export const Logs = () => {
                 </span>
               </div>
             ))}
-            <div ref={messagesEndRef} />
+            <div className="mt-6" ref={messagesEndRef} />
           </div>
-          <Checkbox
-            label="Scroll to bottom on new message"
-            value={settings.scrollOnNewLog}
-            setValue={(newValue) => onSetValue("scrollOnNewLog", newValue)}
-          />
-          <Checkbox
-            label="Indent log lines"
-            description="Indent each log line according to their respective starting position."
-            value={settings.indentLogLines}
-            setValue={(newValue) => onSetValue("indentLogLines", newValue)}
-          />
-          <Checkbox
-            label="Hide wrapped text"
-            description="Hides text that is meant to be wrapped."
-            value={settings.hideWrappedText}
-            setValue={(newValue) => onSetValue("hideWrappedText", newValue)}
-          />
         </div>
       </div>
 
       <LogFiles />
+
     </main>
   );
 };
@@ -179,10 +177,8 @@ const LogFiles = () => {
   );
 
   return (
-    <div className="max-w-screen-xl mx-auto pb-12 px-2 sm:px-4 lg:px-8">
-      <div
-        className="bg-white dark:bg-gray-800 rounded-lg shadow-lg px-2 sm:px-4 pt-3 sm:pt-4"
-      >
+    <div className="max-w-screen-xl mx-auto pb-10 px-2 sm:px-4 lg:px-8">
+      <div className="bg-white dark:bg-gray-800 rounded-lg shadow-lg px-2 sm:px-4 pt-3 sm:pt-4">
         <div className="mt-2">
           <h2 className="text-lg leading-6 font-medium text-gray-900 dark:text-white">Log files</h2>
           <p className="mt-1 text-sm text-gray-500 dark:text-gray-400">
@@ -191,7 +187,7 @@ const LogFiles = () => {
         </div>
 
         {data && data.files.length > 0 ? (
-          <section className="py-6 light:bg-white dark:bg-gray-800 light:shadow sm:rounded-md">
+          <section className="py-3 light:bg-white dark:bg-gray-800 light:shadow sm:rounded-md">
             <ol className="min-w-full relative">
               <li className="hidden sm:grid grid-cols-12 gap-4 mb-2 border-b border-gray-200 dark:border-gray-700">
                 <div className="col-span-5 px-2 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-400 uppercase tracking-wider">
@@ -241,7 +237,7 @@ const LogFilesItem = ({ file }: LogFilesItemProps) => {
           {simplifyDate(file.updated_at)}
         </div>
 
-        <div className="col-span-1 hidden sm:flex items-center text-sm font-medium text-gray-900 dark:text-white">
+        <div className="col-span-1 hidden sm:flex items-center justify-center text-sm font-medium text-gray-900 dark:text-white">
           <Link
             className={classNames(
               "text-gray-900 dark:text-gray-300",
@@ -257,5 +253,75 @@ const LogFilesItem = ({ file }: LogFilesItemProps) => {
         </div>
       </div>
     </li>
+  );
+};
+
+// interface LogsDropdownProps {}
+
+const LogsDropdown = () => {
+  const [settings, setSettings] = SettingsContext.use();
+
+  const onSetValue = (
+    key: "scrollOnNewLog" | "indentLogLines" | "hideWrappedText",
+    newValue: boolean
+  ) => setSettings((prevState) => ({
+    ...prevState,
+    [key]: newValue
+  }));
+
+  return (
+    <Menu as="div">
+      <Menu.Button className="px-4 py-2">
+        <Cog6ToothIcon
+          className="w-5 h-5 text-gray-700 hover:text-gray-900 dark:text-gray-100 dark:hover:text-gray-400"
+          aria-hidden="true"
+        />
+      </Menu.Button>
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items
+          className="absolute right-0 mt-1 origin-top-right bg-white dark:bg-gray-800 divide-y divide-gray-200 dark:divide-gray-700 rounded-md shadow-lg ring-1 ring-black ring-opacity-10 focus:outline-none"
+        >
+          <div className="p-3">
+            <Menu.Item>
+              {({ active }) => (
+                <Checkbox
+                  label="Scroll to bottom on new message"
+                  value={settings.scrollOnNewLog}
+                  setValue={(newValue) => onSetValue("scrollOnNewLog", newValue)}
+                />
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <Checkbox
+                  label="Indent log lines"
+                  description="Indent each log line according to their respective starting position."
+                  value={settings.indentLogLines}
+                  setValue={(newValue) => onSetValue("indentLogLines", newValue)}
+                />
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <Checkbox
+                  label="Hide wrapped text"
+                  description="Hides text that is meant to be wrapped."
+                  value={settings.hideWrappedText}
+                  setValue={(newValue) => onSetValue("hideWrappedText", newValue)}
+                />
+              )}
+            </Menu.Item>
+          </div>
+        </Menu.Items>
+      </Transition>
+    </Menu>
   );
 };
