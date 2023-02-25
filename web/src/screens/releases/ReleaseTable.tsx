5363eeea8c0df18bf238ba2a938debac04dd7957
soup <soup@r4tio.dev>
2023-02-25 15:38:03 +0100

    handling ReleaseTable loading

--- web/src/screens/releases/ReleaseTable.tsx
+++ web/src/screens/releases/ReleaseTable.tsx
@@ -196,7 +196,153 @@ export const ReleaseTable = () => {
     return <p>Error</p>;
 
   if (isLoading)
-    return <p className="animate-pulse text-black dark:text-white">Loading release table...</p>;
+    return (
+      <div className="animate-pulse flex flex-col">
+        <div className="flex mb-6 flex-col sm:flex-row">
+          {headerGroups.map((headerGroup) =>
+            headerGroup.headers.map((column) => (
+              column.Filter ? (
+                <React.Fragment key={column.id}>{column.render("Filter")}</React.Fragment>
+              ) : null
+            ))
+          )}
+        </div>
+        <div className="bg-white shadow-lg dark:bg-gray-800 rounded-md overflow-auto">
+          <table {...getTableProps()} className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
+            <thead className="bg-gray-50 dark:bg-gray-800">
+              {headerGroups.map((headerGroup) => {
+                const { key: rowKey, ...rowRest } = headerGroup.getHeaderGroupProps();
+                return (
+                  <tr key={rowKey} {...rowRest}>
+                    {headerGroup.headers.map((column) => {
+                      const { key: columnKey, ...columnRest } = column.getHeaderProps(column.getSortByToggleProps());
+                      return (
+                      // Add the sorting props to control sorting. For this example
+                      // we can add them into the header props
+                        <th
+                          key={`${rowKey}-${columnKey}`}
+                          scope="col"
+                          className="first:pl-5 pl-3 pr-3 py-3 first:rounded-tl-md last:rounded-tr-md text-xs font-medium tracking-wider text-left text-gray-500 uppercase group"
+                          {...columnRest}
+                        >
+                          <div className="flex items-center justify-between">
+                            <>{column.render("Header")}</>
+                            {/* Add a sort direction indicator */}
+                            <span>
+                              {column.isSorted ? (
+                                column.isSortedDesc ? (
+                                  <Icons.SortDownIcon className="w-4 h-4 text-gray-400" />
+                                ) : (
+                                  <Icons.SortUpIcon className="w-4 h-4 text-gray-400" />
+                                )
+                              ) : (
+                                <Icons.SortIcon className="w-4 h-4 text-gray-400 opacity-0 group-hover:opacity-100" />
+                              )}
+                            </span>
+                          </div>
+                        </th>
+                      );
+                    })}
+                  </tr>
+                );
+              })}
+            </thead>
+            <tbody
+              {...getTableBodyProps()}
+              className="divide-y divide-gray-200 dark:divide-gray-700"
+            >
+              {page.map((row) => {
+                prepareRow(row);
+
+                const { key: bodyRowKey, ...bodyRowRest } = row.getRowProps();
+                return (
+                  <tr key={bodyRowKey} {...bodyRowRest}>
+                    {row.cells.map((cell) => {
+                      const { key: cellRowKey, ...cellRowRest } = cell.getCellProps();
+                      return (
+                        <td
+                          key={cellRowKey}
+                          className="first:pl-5 pl-3 pr-3 whitespace-nowrap"
+                          role="cell"
+                          {...cellRowRest}
+                        >
+                          <>{cell.render("Cell")}</>
+                        </td>
+                      );
+                    })}
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+
+          {/* Pagination */}
+          <div className="flex items-center justify-between px-6 py-3 border-t border-gray-200 dark:border-gray-700">
+            <div className="flex justify-between flex-1 sm:hidden">
+              <DataTable.Button onClick={() => previousPage()} disabled={!canPreviousPage}>Previous</DataTable.Button>
+              <DataTable.Button onClick={() => nextPage()} disabled={!canNextPage}>Next</DataTable.Button>
+            </div>
+            <div className="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
+              <div className="flex items-baseline gap-x-2">
+                <span className="text-sm text-gray-700 dark:text-gray-500">
+                Page <span className="font-medium">{pageIndex + 1}</span> of <span className="font-medium">{pageOptions.length}</span>
+                </span>
+                <label>
+                  <span className="sr-only bg-gray-700">Items Per Page</span>
+                  <select
+                    className="py-1 pl-2 pr-8 text-sm block w-full border-gray-300 rounded-md shadow-sm cursor-pointer dark:bg-gray-800 dark:border-gray-600 dark:text-gray-400 dark:hover:text-gray-500 focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
+                    value={pageSize}
+                    onChange={e => {
+                      setPageSize(Number(e.target.value));
+                    }}
+                  >
+                    {[5, 10, 20, 50].map(pageSize => (
+                      <option key={pageSize} value={pageSize}>
+                      Show {pageSize}
+                      </option>
+                    ))}
+                  </select>
+                </label>
+              </div>
+              <div>
+                <nav className="inline-flex -space-x-px rounded-md shadow-sm" aria-label="Pagination">
+                  <DataTable.PageButton
+                    className="rounded-l-md"
+                    onClick={() => gotoPage(0)}
+                    disabled={!canPreviousPage}
+                  >
+                    <span className="sr-only text-gray-400 dark:text-gray-500 dark:bg-gray-700">First</span>
+                    <ChevronDoubleLeftIcon className="w-4 h-4 text-gray-400 dark:text-gray-500" aria-hidden="true" />
+                  </DataTable.PageButton>
+                  <DataTable.PageButton
+                    onClick={() => previousPage()}
+                    disabled={!canPreviousPage}
+                  >
+                    <span className="sr-only text-gray-400 dark:text-gray-500 dark:bg-gray-700">Previous</span>
+                    <ChevronLeftIcon className="w-4 h-4 text-gray-400 dark:text-gray-500" aria-hidden="true" />
+                  </DataTable.PageButton>
+                  <DataTable.PageButton
+                    onClick={() => nextPage()}
+                    disabled={!canNextPage}>
+                    <span className="sr-only text-gray-400 dark:text-gray-500 dark:bg-gray-700">Next</span>
+                    <ChevronRightIcon className="w-4 h-4 text-gray-400 dark:text-gray-500" aria-hidden="true" />
+                  </DataTable.PageButton>
+                  <DataTable.PageButton
+                    className="rounded-r-md"
+                    onClick={() => gotoPage(pageCount - 1)}
+                    disabled={!canNextPage}
+                  >
+                    <span className="sr-only text-gray-400 dark:text-gray-500 dark:bg-gray-700">Last</span>
+                    <ChevronDoubleRightIcon className="w-4 h-4 text-gray-400 dark:text-gray-500" aria-hidden="true" />
+                  </DataTable.PageButton>
+                </nav>
+              </div>
+            </div>
+          </div>
+        </div>
+        <EmptyListState text="Loading release table..." />
+      </div>
+    );
 
   if (!data)
     return <EmptyListState text="No recent activity" />;
