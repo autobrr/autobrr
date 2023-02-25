5bc93f2998164162770174f0e908b271cd7a42fe
soup <soup@r4tio.cat>
2023-02-25 13:22:43 +0000

    name StatsItems in isLoading

--- web/src/screens/dashboard/Stats.tsx
+++ web/src/screens/dashboard/Stats.tsx
@@ -40,9 +40,9 @@ export const Stats = () => {
         Stats
         </h1>
         <dl className="animate-pulse grid grid-cols-1 gap-5 mt-5 sm:grid-cols-2 lg:grid-cols-3">
-          <StatsItem name="" placeholder="&nbsp;"/>
-          <StatsItem name="" placeholder="&nbsp;"/>
-          <StatsItem name="" placeholder="&nbsp;"/>
+          <StatsItem name="Filtered Releases" placeholder="&nbsp;"/>
+          <StatsItem name="Rejected Pushes" placeholder="&nbsp;"/>
+          <StatsItem name="Approved Pushes" placeholder="&nbsp;"/>
         </dl>
       </div>
     );
