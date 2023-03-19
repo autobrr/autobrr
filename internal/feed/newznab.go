175d5895fce919264e0d1ffacabe5b6bf484cb45
Kyle Sanderson <kyle.leet@gmail.com>
2023-03-19 13:14:36 -0700

    continue newznab

--- internal/feed/newznab.go
+++ internal/feed/newznab.go
@@ -80,7 +80,7 @@ func (j *NewznabJob) process(ctx context.Context) error {
 		if j.Feed.MaxAge > 0 {
 			if item.PubDate.After(time.Date(1970, time.April, 1, 0, 0, 0, 0, time.UTC)) {
 				if !isNewerThanMaxAge(j.Feed.MaxAge, item.PubDate.Time, now) {
-					return nil
+					continue
 				}
 			}
 		}
