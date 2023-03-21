4ca4f6f3a7a70b01a276eb8d3e83dfb5aa1fa917
soup <soup@r4tio.dev>
2023-03-21 01:20:10 +0100

    refactor log sanitization for better performance

--- internal/http/logs.go
+++ internal/http/logs.go
@@ -1,15 +1,20 @@
 package http
 
 import (
+	"bufio"
+	"io"
 	"io/fs"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 	"path"
 	"path/filepath"
 	"regexp"
+	"runtime"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/autobrr/autobrr/internal/config"
@@ -87,7 +92,8 @@ func (h logsHandler) files(w http.ResponseWriter, r *http.Request) {
 	render.JSON(w, r, response)
 }
 
-var ( // regexes for sanitizing log files
+var (
+	// regexes for sanitizing log files
 	keyValueRegex = regexp.MustCompile(`(torrent_pass|passkey|authkey|secret_key|apikey)=([a-zA-Z0-9]+)`)
 	combinedRegex = regexp.MustCompile(`(https?://[^\s]+/((rss/download/[a-zA-Z0-9]+/)|torrent/download/((auto\.[a-zA-Z0-9]+\.|[a-zA-Z0-9]+\.))))([a-zA-Z0-9]+)`)
 	inviteRegex   = regexp.MustCompile(`(Voyager autobot [\p{L}0-9]+ |Satsuki enter #announce [\p{L}0-9]+ |Millie announce |DBBot announce |ENDOR !invite [\p{L}0-9]+ |Vertigo ENTER #GGn-Announce [\p{L}0-9]+ |midgards announce |HeBoT !invite |NBOT !invite |Muffit bot #nbl-announce [\p{L}0-9]+ |hermes enter #announce [\p{L}0-9]+ |LiMEY_ !invite |PS-Info pass |PT-BOT invite |Hummingbird ENTER [\p{L}0-9]+ |Drone enter #red-announce [\p{L}0-9]+ |SceneHD \.invite |erica letmeinannounce [\p{L}0-9]+ |Synd1c4t3 invite |UHDBot invite |Sauron bot #ant-announce [\p{L}0-9]+ |RevoTT !invite [\p{L}0-9]+ |Cerberus identify [\p{L}0-9]+ )([\p{L}0-9]+)`)
@@ -95,40 +101,102 @@ var ( // regexes for sanitizing log files
 	saslRegex     = regexp.MustCompile(`(--> AUTHENTICATE )([\p{L}0-9!#%&*+/:;<=>?@^_` + "`" + `{|}~]+)`)
 )
 
+// ProcessLines is a worker function that processes a batch of lines using regular expressions.
+func ProcessLines(lines []string) []string {
+	var result []string
+
+	for _, line := range lines {
+		// Sanitize the line using regular expressions
+		line = keyValueRegex.ReplaceAllString(line, "${1}=REDACTED")
+		line = combinedRegex.ReplaceAllString(line, "${1}REDACTED")
+		line = inviteRegex.ReplaceAllString(line, "${1}REDACTED")
+		line = nickservRegex.ReplaceAllString(line, "${1}REDACTED")
+		line = saslRegex.ReplaceAllString(line, "${1}REDACTED")
+
+		result = append(result, line)
+	}
+
+	return result
+}
+
+// SanitizeLogFile reads a log file line by line and sanitizes each line using regular expressions.
+// It uses a worker pool to process multiple lines concurrently.
 func SanitizeLogFile(filePath string) (string, error) {
-	data, err := ioutil.ReadFile(filePath)
+	inFile, err := os.Open(filePath)
+	if err != nil {
+		return "", err
+	}
+	defer inFile.Close()
+
+	reader := bufio.NewReader(inFile)
+
+	outFile, err := ioutil.TempFile("", "sanitized-log-*.log")
+	if err != nil {
+		return "", err
+	}
+	defer outFile.Close()
+
+	writer := bufio.NewWriterSize(outFile, 2*1024*1024)
+
+	// Define the number of worker goroutines
+	numWorkers := runtime.NumCPU()
+
+	// Create a channel to communicate between workers and the main goroutine
+	linesCh := make(chan string, numWorkers)
+
+	// Create a WaitGroup to wait for all workers to finish
+	wg := sync.WaitGroup{}
+
+	// Start the worker goroutines
+	for i := 0; i < numWorkers; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+
+			for line := range linesCh {
+				// Sanitize the line using regular expressions
+				line = keyValueRegex.ReplaceAllString(line, "${1}=REDACTED")
+				line = combinedRegex.ReplaceAllString(line, "${1}REDACTED")
+				line = inviteRegex.ReplaceAllString(line, "${1}REDACTED")
+				line = nickservRegex.ReplaceAllString(line, "${1}REDACTED")
+				line = saslRegex.ReplaceAllString(line, "${1}REDACTED")
+
+				// Write the sanitized line to the output file
+				_, err := writer.WriteString(line)
+				if err != nil {
+					log.Printf("Error writing line to output file: %v", err)
+					return
+				}
+			}
+		}()
+	}
+
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil && err != io.EOF {
+			return "", err
+		}
+		if len(line) == 0 {
+			break
+		}
+
+		// Send the line to the channel for processing by a worker
+		linesCh <- line
+	}
+
+	// Close the channel to signal to the workers that all lines have been sent
+	close(linesCh)
+
+	// Wait for all workers to finish
+	wg.Wait()
+
+	// Flush the buffer to write any remaining data to the output file
+	err = writer.Flush()
 	if err != nil {
 		return "", err
 	}
 
-	sanitizedData := string(data)
-
-	// torrent_pass, passkey, authkey, secret_key, apikey, rsskey
-	sanitizedData = keyValueRegex.ReplaceAllString(sanitizedData, "${1}=REDACTED")
-	sanitizedData = combinedRegex.ReplaceAllString(sanitizedData, "${1}REDACTED")
-
-	// irc related
-	sanitizedData = inviteRegex.ReplaceAllString(sanitizedData, "${1}REDACTED")
-	sanitizedData = nickservRegex.ReplaceAllString(sanitizedData, "${1}REDACTED")
-	sanitizedData = saslRegex.ReplaceAllString(sanitizedData, "${1}REDACTED")
-
-	tmpFile, err := ioutil.TempFile("", "sanitized-log-*.log")
-	if err != nil {
-		return "", err
-	}
-
-	_, err = tmpFile.WriteString(sanitizedData)
-	if err != nil {
-		tmpFile.Close()
-		return "", err
-	}
-
-	err = tmpFile.Close()
-	if err != nil {
-		return "", err
-	}
-
-	return tmpFile.Name(), nil
+	return outFile.Name(), nil
 }
 
 func (h logsHandler) downloadFile(w http.ResponseWriter, r *http.Request) {
