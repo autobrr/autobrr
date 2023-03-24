ea5e8b6a04e52468afde7bb7f8b83c8b1603e9d3
KaiserBh <kaiserbh@proton.me>
2023-03-25 07:38:37 +1100

    refactored and default port to 7474, don't think we need the rollUpOptions.

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -19,32 +19,12 @@ export default ({ mode }: { mode: any }) => {
       hmr: {
         overlay: true
       },
-      port: 3000
+      port: 7474
     },
     build: {
-      // outDir: "build",
+
       manifest: true,
-      // assetsDir: "static",
-      // cssCodeSplit: true,
-      sourcemap: true
-      // rollupOptions: {
-      //   output: {
-      //     entryFileNames: "static/js/[name].[hash].js",
-      //     chunkFileNames: "static/js/[name].[hash].js",
-      //     assetFileNames: (assetInfo) => {
-      //       const extension = assetInfo.name?.split(".").pop();
-      //       switch (extension) {
-      //       case "css":
-      //         return "static/css/[name].[hash].css";
-      //       case "woff2":
-      //         return "static/media/[name].[hash].[ext]";
-      //       default:
-      //         return "static/[name].[hash].[ext]";
-      //       }
-      //     }
-      //   }
-      // }
-      
+      sourcemap: true      
     }
   });
 };
