edae1bbf4b2c61f0ea0aa79eeabbcef7a7cb9dc3
KaiserBh <41852205+KaiserBh@users.noreply.github.com>
2023-04-08 00:04:10 +1000

    refactor(web): migrate create-react-app to vite (#787)
    * removed react-app type instead use vite.
    
    * removed index.html from public since vite uses it from root: read more: https://vitejs.dev/guide/#index-html-and-project-root
    
    * yarn.lock update.
    
    * added vite config file. With commented rollUp option if we want the build to be called build but using default stuff for now.
    
    * updated tsconfig to use vite and include vite.config.ts
    
    * changed package json build commands to use vite.
    
    * for some reason there is an error in vite config when we put project as tsconfig.json.
    
    * build.go updated to use the new dist folder.
    
    * refactored as well updated to use dist and web.AssetHandler again.
    
    * Fixed issue forcing the frontend to be reloaded for all routes to work if logged in fresh without reloading it will always go back to dashboard.
    
    * updated it to use the new function; need to fix the Index for baseUrl I believe, if enabled it works except logs route will crash due to cors.
    
    * refactored and default port to 7474, don't think we need the rollUpOptions.
    
    * added tmp/ to ignore .
    
    * init air.toml, for dev hot reloading both app and backend. https://github.com/cosmtrek/air run it using air but make sure it's in PATH
    
    * updated the start command to build and watch for changes, works great with air.
    
    * revert
    
    * added proxy for vite config. To be used for dev.
    
    * refactor: I think this should fix it, when logs route etc getting accessed usually it throws error but by getting rid of the catch-all it should work as intended, since web.RegisterHandler(r) will catch the unmatched ones.
    
    * fix: baseurl and build
    
    * fix(build): docker ignore !web/dist
    
    * fix(build): dockerignore add exclusions
    
    * docs: update README.md
    
    * build: update postcss config
    
    ---------
    
    Co-authored-by: KaiserBh <kaiserbh@proton.me>
    Co-authored-by: ze0s <ze0s@riseup.net>

--- web/postcss.config.js
+++ web/postcss.config.js
@@ -1,6 +1,8 @@
 module.exports = {  
-    plugins: {    
-        tailwindcss: {},    
-        autoprefixer: {},  
-    }
+  plugins: {
+    "postcss-import": {},
+    "tailwindcss/nesting": {},
+    tailwindcss: {},    
+    autoprefixer: {},  
+  }
 }
