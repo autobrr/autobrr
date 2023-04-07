1becf5db830ee03bb78941c601f2b11fac6cddf5
ze0s <43699394+zze0s@users.noreply.github.com>
2023-04-07 19:07:48 +0200

    Revert "refactor(web): migrate create-react-app to vite (#787)"
    This reverts commit edae1bbf4b2c61f0ea0aa79eeabbcef7a7cb9dc3.

--- web/postcss.config.js
+++ web/postcss.config.js
@@ -1,8 +1,6 @@
 module.exports = {  
-  plugins: {
-    "postcss-import": {},
-    "tailwindcss/nesting": {},
-    tailwindcss: {},    
-    autoprefixer: {},  
-  }
+    plugins: {    
+        tailwindcss: {},    
+        autoprefixer: {},  
+    }
 }
