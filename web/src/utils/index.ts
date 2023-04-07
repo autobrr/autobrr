807f9a17d5c08ed7d5400392b8a9d794d14a913e
KaiserBh <kaiserbh@proton.me>
2023-04-08 05:00:16 +1000

    [DEV-821] wip:fix vite, this seem to be working fine.

--- web/src/utils/index.ts
+++ web/src/utils/index.ts
@@ -9,17 +9,8 @@ export function sleep(ms: number) {
 export function baseUrl() {
   let baseUrl = "";
   if (window.APP.baseUrl) {
-    if (window.APP.baseUrl === "/") {
-      baseUrl = "/";
-    } else if (window.APP.baseUrl === "{{.BaseUrl}}") {
-      baseUrl = "/";
-    } else if (window.APP.baseUrl === "/autobrr/") {
-      baseUrl = "/autobrr/";
-    } else {
-      baseUrl = window.APP.baseUrl;
-    }
+    baseUrl = window.APP.baseUrl;
   }
-
   return baseUrl;
 }
 
