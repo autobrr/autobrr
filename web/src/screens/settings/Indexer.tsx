0a3dbd31a5f81011aefddc004cfdc9da97b904fd
Kyle Sanderson <kyle.leet@gmail.com>
2023-04-29 20:00:58 -0700

    oh right.

--- web/src/screens/settings/Indexer.tsx
+++ web/src/screens/settings/Indexer.tsx
@@ -181,7 +181,7 @@ function IndexerSettings() {
               Indexers
             </h3>
             <p className="mt-1 text-sm text-gray-500 dark:text-gray-400">
-              Indexer settings for IRC, RSS, Newznab, and Torznab based indexers.<br>
+              Indexer settings for IRC, RSS, Newznab, and Torznab based indexers.</br>
               Generic feeds can be added here by selecting the Generic indexer.
             </p>
           </div>
