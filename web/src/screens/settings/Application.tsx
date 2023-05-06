b05caba816d04dcb9cdc3606121893180cd9078c
soup <soup@r4tio.dev>
2023-05-06 13:01:47 +0200

    improved toast for update checks

--- web/src/screens/settings/Application.tsx
+++ web/src/screens/settings/Application.tsx
@@ -100,13 +100,10 @@ function ApplicationSettings() {
     }
   });
 
-  const toggleCheckUpdateMutation = useMutation({
-    mutationFn: (value: boolean) => APIClient.config.update({ check_for_updates: value }),
-    onSuccess: () => {
-      toast.custom((t) => <Toast type="success" body={"Config successfully updated!"} t={t}/>);
-
+  const toggleCheckUpdateMutation = useMutation((value: boolean) => APIClient.config.update({ check_for_updates: value }).then(() => value), {
+    onSuccess: (value: boolean) => {
+      toast.custom(t => <Toast type="success" body={`Check for updates has been ${value ? "enabled" : "disabled"}. ${value ? "You will now be notified of new updates." : "You won't be notified of new updates."}`} t={t} />);
       queryClient.invalidateQueries({ queryKey: ["config"] });
-
       checkUpdateMutation.mutate();
     }
   });
