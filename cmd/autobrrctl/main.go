17460f605ccf9f16dfcc0c787d7dbf15bc2c42e8
soup <soup@r4tio.dev>
2023-05-16 12:07:30 +0000

    refactor: only migrate data, no schema

--- cmd/autobrrctl/main.go
+++ cmd/autobrrctl/main.go
@@ -423,12 +423,6 @@ func migrate(sqliteDBPath, postgresDBURL string) {
 	}
 	defer postgresDB.Close()
 
-	// Create PostgreSQL schema
-	_, err = postgresDB.Exec(postgresSchema)
-	if err != nil {
-		log.Fatalf("Failed to create PostgreSQL schema: %v", err)
-	}
-
 	// List of table names to migrate
 	tables := []string{
 		"users", "indexer", "irc_network", "irc_channel", "filter", "filter_indexer", "client", "action", "release", "release_action_status", "notification", "feed", "feed_cache", "api_key",
