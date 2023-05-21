6c6995e426fa74a788f520204d0cf5300a30b24e
ze0s <ze0s@riseup.net>
2023-05-21 18:28:14 +0200

    refactor: split into component and add confirmation modal
    also restyle component

--- web/src/screens/settings/Releases.tsx
+++ web/src/screens/settings/Releases.tsx
@@ -3,38 +3,76 @@
  * SPDX-License-Identifier: GPL-2.0-or-later
  */
 
-import { useState } from "react";
+import React, { useRef, useState } from "react";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { toast } from "react-hot-toast";
 
 import { APIClient } from "@api/APIClient";
 import Toast from "@components/notifications/Toast";
 import { releaseKeys } from "@screens/releases/ReleaseTable";
+import { useToggle } from "@hooks/hooks";
+import { DeleteModal } from "@components/modals";
 
 function ReleaseSettings() {
+  return (
+    <div className="lg:col-span-9">
+      <div className="py-6 px-4 sm:p-6 lg:pb-8">
+        <div>
+          <h2 className="text-lg leading-6 font-medium text-gray-900 dark:text-white">
+            Releases
+          </h2>
+          <p className="mt-1 text-sm text-gray-500 dark:text-gray-400">
+            Manage release history.
+          </p>
+        </div>
+      </div>
 
+      <div className="py-6 px-4">
+        <div className="border border-red-500 rounded">
+          <div className="py-6 px-4 sm:p-6 lg:pb-8">
+            <div>
+              <h2 className="text-lg leading-6 font-medium text-gray-900 dark:text-white">Danger zone</h2>
+              <p className="mt-1 text-sm text-gray-500 dark:text-gray-400">
+                This will clear release history in your database
+              </p>
+            </div>
+          </div>
+
+          <div className="py-6 px-4 sm:p-6 lg:pb-8">
+            <DeleteReleases />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+const getDurationLabel = (durationValue: number): string => {
+  const durationOptions: Record<number, string> = {
+    0: "all time",
+    1: "1 hour",
+    12: "12 hours",
+    24: "1 day",
+    168: "1 week",
+    720: "1 month",
+    2160: "3 months",
+    4320: "6 months",
+    8760: "1 year"
+  };
+
+  return durationOptions[durationValue] || "Invalid duration";
+};
+
+function DeleteReleases() {
   const queryClient = useQueryClient();
   const [duration, setDuration] = useState<string>("");
-
-  const getDurationLabel = (durationValue: number): string => {
-    const durationOptions: Record<number, string> = {
-      1: "1 hour",
-      12: "12 hours",
-      24: "1 day",
-      168: "1 week",
-      720: "1 month",
-      2160: "3 months",
-      4320: "6 months",
-      8760: "1 year"
-    };
-
-    return durationOptions[durationValue] || "Invalid duration";
-  };
+  const [parsedDuration, setParsedDuration] = useState<number>(0);
+  const cancelModalButtonRef = useRef<HTMLInputElement | null>(null);
+  const [deleteModalIsOpen, toggleDeleteModal] = useToggle(false);
 
   const deleteOlderMutation = useMutation({
     mutationFn: (duration: number) => APIClient.release.deleteOlder(duration),
     onSuccess: () => {
-      const parsedDuration = parseInt(duration, 10);
       if (parsedDuration === 0) {
         toast.custom((t) => (
           <Toast type="success" body={"All releases were deleted."} t={t} />
@@ -51,77 +89,61 @@ function ReleaseSettings() {
   });
 
   const deleteOlderReleases = () => {
-    const parsedDuration = parseInt(duration, 10);
-
     if (isNaN(parsedDuration) || parsedDuration < 0) {
       toast.custom((t) => <Toast type="error" body={"Please select a valid duration."} t={t} />);
-    } else {
-      deleteOlderMutation.mutate(parsedDuration);
+      return;
     }
+
+    deleteOlderMutation.mutate(parsedDuration);
   };
 
   return (
-    <form
-      className="lg:col-span-9"
-      action="#"
-      method="POST"
-    >
-      <div className="py-6 px-4 sm:p-6 lg:pb-8">
-        <div>
-          <h2 className="text-lg leading-6 font-medium text-gray-900 dark:text-white">
-            Releases
-          </h2>
-          <p className="mt-1 text-sm text-gray-500 dark:text-gray-400">
-            Manage release history.
-          </p>
-        </div>
-      </div>
+    <div className="flex justify-between items-center rounded-md shadow-sm">
+      <DeleteModal
+        isOpen={deleteModalIsOpen}
+        toggle={toggleDeleteModal}
+        buttonRef={cancelModalButtonRef}
+        deleteAction={deleteOlderReleases}
+        title="Remove releases"
+        text={`Are you sure you want to remove releases older than ${getDurationLabel(parsedDuration)}? This action cannot be undone.`}
+      />
 
-      <div className="pb-6 divide-y divide-gray-200 dark:divide-gray-700">
-        <div className="px-4 py-5">
-          <div className="text-center">
-            <h3 className="sm:text-lg leading-6 font-medium text-gray-900 dark:text-white">
-              Danger Zone
-            </h3>
-            <p className="mt-1 text-sm text-gray-900 dark:text-white">
-              This will clear release history in your database
-            </p>
-          </div>
-          <div className="mt-8 max-w-sm mx-auto">
-            <label htmlFor="duration" className="block text-sm text-gray-700 dark:text-white">
-              Delete releases older than:
-            </label>
-            <div className="flex items-center mt-2 rounded-md shadow-sm">
-              <select
-                name="duration"
-                id="duration"
-                className="focus:outline-none focus:ring-1 focus:ring-offset-0 focus:ring-blue-500 dark:focus:ring-blue-500 flex-1 block w-full rounded-md sm:text-sm border-gray-300 dark:border-gray-600 dark:bg-gray-800 dark:text-white"
-                value={duration}
-                onChange={(e) => setDuration(e.target.value)}
-              >
-                <option value="">Select duration</option>
-                <option value="1">1 hour</option>
-                <option value="12">12 hours</option>
-                <option value="24">1 day</option>
-                <option value="168">1 week</option>
-                <option value="720">1 month</option>
-                <option value="2160">3 months</option>
-                <option value="4320">6 months</option>
-                <option value="8760">1 year</option>
-                <option value="0">Delete everything</option>
-              </select>
-              <button
-                type="button"
-                onClick={deleteOlderReleases}
-                className="ml-2 inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-red-700 hover:text-red-800 dark:text-white bg-red-200 dark:bg-red-700 hover:bg-red-300 dark:hover:bg-red-800 focus:outline-none focus:ring-1 focus:ring-inset focus:ring-red-600"
-              >
-                Delete
-              </button>
-            </div>
-          </div>
-        </div>
+      <label htmlFor="duration" className="flex flex-col">
+        <p className="text-sm font-medium text-gray-900 dark:text-white">Delete</p>
+        <p className="text-sm text-gray-500 dark:text-gray-400">Delete releases older than select duration</p>
+      </label>
+      <div>
+        <select
+          name="duration"
+          id="duration"
+          className="focus:outline-none focus:ring-1 focus:ring-offset-0 focus:ring-blue-500 dark:focus:ring-blue-500 rounded-md sm:text-sm border-gray-300 dark:border-gray-600 dark:bg-gray-800 dark:text-white"
+          value={duration}
+          onChange={(e) => {
+            const parsedDuration = parseInt(e.target.value, 10);
+            setParsedDuration(parsedDuration);
+            setDuration(e.target.value);
+          }}
+        >
+          <option value="">Select duration</option>
+          <option value="1">1 hour</option>
+          <option value="12">12 hours</option>
+          <option value="24">1 day</option>
+          <option value="168">1 week</option>
+          <option value="720">1 month</option>
+          <option value="2160">3 months</option>
+          <option value="4320">6 months</option>
+          <option value="8760">1 year</option>
+          <option value="0">Delete everything</option>
+        </select>
+        <button
+          type="button"
+          onClick={toggleDeleteModal}
+          className="ml-2 inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-red-700 hover:text-red-800 dark:text-white bg-red-200 dark:bg-red-700 hover:bg-red-300 dark:hover:bg-red-800 focus:outline-none focus:ring-1 focus:ring-inset focus:ring-red-600"
+        >
+          Delete
+        </button>
       </div>
-    </form>
+    </div>
   );
 }
 
