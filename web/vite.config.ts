58d4f4123f70e11074a1823fe2385fb0fc22d329
Fabricio Silva <hi@fabricio.dev>
2023-06-03 09:26:34 +0100

    install vite-plugin-svgr to import svg as react components

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,6 +3,8 @@ import { fileURLToPath, URL } from "node:url";
 import react from "@vitejs/plugin-react-swc";
 import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig, loadEnv } from "vite";
+import svgr from "vite-plugin-svgr";
+
 // https://vitejs.dev/config/
 export default ({ mode }: { mode: any }) => {
   // early load .env file
@@ -11,7 +13,7 @@ export default ({ mode }: { mode: any }) => {
 
   return defineConfig({
     base: "",
-    plugins: [react(), VitePWA({
+    plugins: [react(), svgr(), VitePWA({
       registerType: "autoUpdate",
       injectRegister: "inline",
       scope: "{{.BaseUrl}}",
