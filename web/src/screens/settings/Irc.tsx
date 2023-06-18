00642e08796c2e07e713510f0a7165c0cca268f3
martylukyy <35452459+martylukyy@users.noreply.github.com>
2023-06-18 20:38:35 +0200

    fullscreen view for announce logs

--- web/src/screens/settings/Irc.tsx
+++ web/src/screens/settings/Irc.tsx
@@ -598,7 +598,30 @@ interface EventsProps {
 export const Events = ({ network, channel }: EventsProps) => {
   const [settings] = SettingsContext.use();
 
+  const [isFullscreen, setIsFullscreen] = useState(false);
   const messagesEndRef = useRef<HTMLDivElement>(null);
+
+  const toggleFullscreen = () => {
+    setIsFullscreen(!isFullscreen);
+  };
+
+  const exitFullscreen = () => {
+    setIsFullscreen(false);
+  };
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        exitFullscreen();
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
 
   const [logs, setLogs] = useState<IrcEvent[]>([]);
 
@@ -644,11 +667,15 @@ export const Events = ({ network, channel }: EventsProps) => {
   }, [settings]);
 
   return (
-    <div className="dark:bg-gray-800 rounded-lg shadow-lg p-1.5">
+    <div className={`dark:bg-gray-800 rounded-lg shadow-lg p-2 ${isFullscreen ? "fixed top-0 left-0 w-screen h-screen z-50" : ""}`}>
       <div className="flex relative">
+        <button className={`dark:bg-gray-800 p-2 absolute top-2 right-2 bg-gray-200 hover:bg-gray-300 dark:hover:bg-gray-700 hover:cursor-pointer rounded-md ${isFullscreen ? "mt-2 mr-4" : "mr-2"}`} onClick={toggleFullscreen}>
+          {isFullscreen
+            ? <span className="flex items-center"><ArrowsPointingInIcon className="w-5 h-5"/></span>
+            : <span className="flex items-center"><ArrowsPointingOutIcon className="w-5 h-5"/></span>}
+        </button>
       </div>
-
-      <div className="overflow-y-auto px-2 rounded-lg h-[60vh] min-w-full bg-gray-100 dark:bg-gray-900 overflow-auto">
+      <div className={`overflow-y-auto rounded-lg min-w-full bg-gray-100 dark:bg-gray-900 overflow-auto ${isFullscreen ? " max-w-full h-full border-8 p-2 border-gray-300 dark:border-gray-700" : "px-2 py-1 aspect-[2/1]"}`}>
         {logs.map((entry, idx) => (
           <div
             key={idx}
