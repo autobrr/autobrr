32ffc875b0d53831c2e4ead5bbb4101684eb56e3
Fabricio Silva <hi@fabricio.dev>
2023-07-02 13:17:10 +0100

    feat(web): use svg logo instead of png (#970)
    * add fonts files to workbox
    
    * remove logo.png and touches manifest assets
    
    * install vite-plugin-svgr to import svg as react components
    
    * move logo.svg from public to src
    
    * replace logo.png to logo.svg
    
    ---------
    
    Co-authored-by: ze0s <43699394+zze0s@users.noreply.github.com>

--- web/src/screens/Base.tsx
+++ web/src/screens/Base.tsx
@@ -12,7 +12,7 @@ import { useMutation, useQuery } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 
 import { AuthContext } from "@utils/Context";
-import logo from "@app/logo.png";
+import { ReactComponent as Logo } from "@app/logo.svg";
 import { APIClient } from "@api/APIClient";
 import Toast from "@components/notifications/Toast";
 import { classNames } from "@utils";
@@ -70,16 +70,7 @@ export default function Base() {
                   <div className="flex items-center">
                     <div className="flex-shrink-0 flex items-center">
                       <Link to="/">
-                        <img
-                          className="block lg:hidden h-10 w-auto"
-                          src={logo}
-                          alt="Logo"
-                        />
-                        <img
-                          className="hidden lg:block h-10 w-auto"
-                          src={logo}
-                          alt="Logo"
-                        />
+                        <Logo className="h-10" />
                       </Link>
                     </div>
                     <div className="sm:ml-3 hidden sm:block">
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,6 +3,8 @@ import { fileURLToPath, URL } from "node:url";
 import react from "@vitejs/plugin-react-swc";
 import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig, loadEnv } from "vite";
+import svgr from "vite-plugin-svgr";
+
 // https://vitejs.dev/config/
 export default ({ mode }: { mode: any }) => {
   // early load .env file
@@ -11,20 +13,16 @@ export default ({ mode }: { mode: any }) => {
 
   return defineConfig({
     base: "",
-    plugins: [react(), VitePWA({
+    plugins: [react(), svgr(), VitePWA({
       registerType: "autoUpdate",
       injectRegister: "inline",
       scope: "{{.BaseUrl}}",
       // strategies: "injectManifest",
       useCredentials: true,
       includeAssets: [
-        "favicon.svg",
-        "favicon.ico",
-        "robots.txt",
-        "logo.png",
-        "apple-touch-icon-*.png",
-        "manifest.webmanifest",
-        "assets/**/*"
+        // looks inside "public" folder 
+        // manifest's icons are automatic added
+        "favicon.ico"
       ],
       manifest: {
         name: "autobrr",
@@ -34,29 +32,28 @@ export default ({ mode }: { mode: any }) => {
         background_color: "#141415",
         icons: [
           {
-            src: "logo.png",
-            sizes: "192x192",
-            type: "image/png"
-          },
-          {
             src: "logo192.png",
             sizes: "192x192",
             type: "image/png"
           },
           {
-            src: "logo512.png",
-            sizes: "512x512",
+            src: "apple-touch-icon-iphone-60x60.png",
+            sizes: "60x60",
             type: "image/png"
           },
           {
-            src: "logo512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any maskable"
+            src: "apple-touch-icon-ipad-76x76.png",
+            sizes: "76x76",
+            type: "image/png"
           },
           {
             src: "apple-touch-icon-iphone-retina-120x120.png",
             sizes: "120x120",
+            type: "image/png"
+          },
+          {
+            src: "apple-touch-icon-ipad-retina-152x152.png",
+            sizes: "152x152",
             type: "image/png"
           }
         ],
@@ -65,7 +62,8 @@ export default ({ mode }: { mode: any }) => {
         display: "standalone"
       },
       workbox: {
-        globPatterns: ["**/*.{js,css,html,svg}"],
+        // looks inside "dist" folder
+        globPatterns: ["**/*.{js,css,html,svg,woff2}"],
         sourcemap: true,
         navigateFallbackDenylist: [/^\/api/]
       }
