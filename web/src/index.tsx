7d945891bdc9cfd2c2d4e10c3b51ee5d3b1b9abb
stacksmash76 <98354295+stacksmash76@users.noreply.github.com>
2023-09-08 13:10:57 +0200

    enhancement: improve font loading performance and avoid page reflow
    chore: get rid of postcss-import (we're not using it and it's not needed for tailwind)

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -6,7 +6,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import "@fontsource-variable/inter";
 import "./index.css";
 import "react-tooltip/dist/react-tooltip.css";
 
