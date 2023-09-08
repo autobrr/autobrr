0687994d55a9069d45af317d109b13b433dfe433
martylukyy <35452459+martylukyy@users.noreply.github.com>
2023-09-08 22:07:53 +0200

    add license header to new files

--- web/src/components/header/Header.tsx
+++ web/src/components/header/Header.tsx
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2021 - 2023, Ludvig Lundgren and the autobrr contributors.
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
 import toast from "react-hot-toast";
 import { useMutation, useQuery } from "@tanstack/react-query";
 import { Disclosure } from "@headlessui/react";
--- web/src/components/header/RightNav.tsx
+++ web/src/components/header/RightNav.tsx
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2021 - 2023, Ludvig Lundgren and the autobrr contributors.
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
 import { Fragment } from "react";
 import { Link } from "react-router-dom";
 import { UserIcon } from "@heroicons/react/24/solid";
