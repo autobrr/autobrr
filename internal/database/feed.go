81e1133dd7d347985351a57be54c090a19cd9898
KaiserBh <kaiserbh@pm.me>
2023-09-14 12:21:50 +1000

    chore(feed): handle error when nothing is deleted.

--- internal/database/feed.go
+++ internal/database/feed.go
@@ -387,9 +387,15 @@ func (r *FeedRepo) Delete(ctx context.Context, id int) error {
 		return errors.Wrap(err, "error building query")
 	}
 
-	_, err = r.db.handler.ExecContext(ctx, query, args...)
+	result, err := r.db.handler.ExecContext(ctx, query, args...)
 	if err != nil {
 		return errors.Wrap(err, "error executing query")
+	}
+
+	if rowsAffected, err := result.RowsAffected(); err != nil {
+		return errors.Wrap(err, "error getting rows affected")
+	} else if rowsAffected == 0 {
+		return domain.ErrRecordNotFound
 	}
 
 	r.log.Debug().Msgf("feed.delete: successfully deleted: %v", id)
