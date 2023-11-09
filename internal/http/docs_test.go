43f0a5c131a9cd2bf2740c01b25b23374bab37bf
soup <soup@r4tio.dev>
2023-11-09 10:03:27 +0100

    feat: Improved URL Scanning

--- internal/http/docs_test.go
+++ internal/http/docs_test.go
@@ -1,26 +1,80 @@
 package http
 
 import (
+	"io/fs"
+	"io/ioutil"
 	"net/http"
+	"path/filepath"
+	"regexp"
+	"strings"
 	"testing"
+	"time"
 )
 
-func TestAutobrrLinks(t *testing.T) {
-	urls := map[string]string{
-		"dedicated":         "https://autobrr.com/configuration/download-clients/dedicated",
-		"shared-seedboxes":  "https://autobrr.com/configuration/download-clients/shared-seedboxes",
-		"irc":               "https://autobrr.com/configuration/irc",
-		"faqs":              "https://autobrr.com/faqs",
-		"actions":           "https://autobrr.com/filters/actions",
-		"categories":        "https://autobrr.com/filters/categories",
-		"examples":          "https://autobrr.com/filters/examples",
-		"freeleech":         "https://autobrr.com/filters/freeleech",
-		"macros":            "https://autobrr.com/filters/macros",
-		"filters":           "https://autobrr.com/filters",
+// Function to extract all autobrr.com URLs from a string using regex.
+func extractAutobrrURLs(fileContent string) []string {
+	// This regex pattern matches URLs with autobrr.com and any path.
+	autobrrURLRegex := regexp.MustCompile(`https?://autobrr\.com/[^ \s"')]+`)
+	matches := autobrrURLRegex.FindAllString(fileContent, -1)
+
+	return matches // Return the matches directly
+}
+
+// Function to read the content of a file and extract autobrr.com URLs.
+func processFile(filePath string) ([]string, error) {
+	content, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return nil, err
 	}
 
-	for name, url := range urls {
-		t.Run(name, func(t *testing.T) {
+	return extractAutobrrURLs(string(content)), nil
+}
+
+// Function to recursively scan directories for .go and .tsx files and test their autobrr.com URLs.
+func TestAutobrrURLsInRepository(t *testing.T) {
+	uniqueURLs := make(map[string]bool)
+
+	// Define the base path where the search should start.
+	basePath := "../.." // Adjust this to the appropriate base path.
+
+	// Walk the entire directory tree starting from the base path.
+	err := filepath.Walk(basePath, func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		// Skip directories.
+		if info.IsDir() {
+			return nil
+		}
+
+		// Check for .go or .tsx files.
+		if filepath.Ext(path) == ".go" || filepath.Ext(path) == ".tsx" || filepath.Ext(path) == ".md" || filepath.Ext(path) == ".yml" {
+			fileURLs, err := processFile(path)
+			if err != nil {
+				t.Errorf("Error processing file %s: %v", path, err)
+				return nil
+			}
+			for _, url := range fileURLs {
+				// Remove the fragment identifier.
+				urlWithoutFragment := strings.Split(url, "#")[0]
+				// Check if the base URL has already been added to the uniqueURLs map.
+				if !uniqueURLs[urlWithoutFragment] {
+					uniqueURLs[urlWithoutFragment] = true // Add the base URL to the map.
+				}
+			}
+		}
+		return nil
+	})
+
+	if err != nil {
+		t.Errorf("Error walking the repository directory tree: %v", err)
+		return
+	}
+
+	// Now test each unique autobrr.com URL
+	for url := range uniqueURLs {
+		t.Run(url, func(t *testing.T) {
 			resp, err := http.Get(url)
 			if err != nil {
 				t.Fatalf("Failed to GET url %s: %v", url, err)
@@ -31,6 +85,8 @@ func TestAutobrrLinks(t *testing.T) {
 			if resp.StatusCode == http.StatusNotFound {
 				t.Errorf("URL %s returned 404 Not Found", url)
 			}
+			// Sleep for a second to help against rate limiting
+			time.Sleep(1 * time.Second)
 		})
 	}
 }
