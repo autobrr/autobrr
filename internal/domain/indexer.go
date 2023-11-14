732bfc0210586ad06eef22fcdbd4cdb007caeacf
Frederick Robinson <frederick.robinson@frrad.com>
2023-11-14 13:10:40 -0800

    add comment about `Test` vs `Tests`

--- internal/domain/indexer.go
+++ internal/domain/indexer.go
@@ -213,6 +213,8 @@ type LineTest struct {
 }
 
 type IndexerIRCParseLine struct {
+	// `Test` is retained for backwards compatibility. Prefer `Tests` for new
+	// definitions.
 	Test    []string   `json:"test"`
 	Tests   []LineTest `json:"tests"`
 	Pattern string     `json:"pattern"`
