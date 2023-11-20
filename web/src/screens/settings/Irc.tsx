213a0986edf1787ac2f79b2a1f86c6c44feb9a9d
KaiserBh <kaiserbh@pm.me>
2023-11-21 08:31:39 +1100

    chore(lint): react-hooks/exhaustive-deps

--- web/src/screens/settings/Irc.tsx
+++ web/src/screens/settings/Irc.tsx
@@ -615,7 +615,7 @@ export const Events = ({ network, channel }: EventsProps) => {
     };
 
     return () => es.close();
-  }, [settings]);
+  }, [channel, network.id, settings]);
 
   const [isFullscreen, toggleFullscreen] = useToggle(false);
 
@@ -643,7 +643,7 @@ export const Events = ({ network, channel }: EventsProps) => {
     };
     if (settings.scrollOnNewLog)
       scrollToBottom();
-  }, [logs]);
+  }, [logs, settings.scrollOnNewLog]);
 
   // Add a useEffect to clear logs div when settings.scrollOnNewLog changes to prevent duplicate entries.
   useEffect(() => {
