cfaeaf0c0f1eb6c1a20d95e9e05601a4b6c01bcd
soup <soup@r4tio.dev>
2023-11-25 21:02:28 +0100

    added LinkIcon to StatsItem
    - Changed grid-cols to 2, as we now have 4 for narrow widths

--- web/src/screens/dashboard/Stats.tsx
+++ web/src/screens/dashboard/Stats.tsx
@@ -7,6 +7,7 @@ import { useQuery } from "@tanstack/react-query";
 import { APIClient } from "@api/APIClient";
 import { classNames } from "@utils";
 import { useNavigate } from "react-router-dom";
+import { LinkIcon } from "@heroicons/react/24/solid";
 
 interface StatsItemProps {
     name: string;
@@ -17,12 +18,15 @@ interface StatsItemProps {
 
 const StatsItem = ({ name, placeholder, value, onClick }: StatsItemProps) => (
   <div
-    className="relative px-4 py-3 cursor-pointer overflow-hidden rounded-lg shadow-lg bg-white dark:bg-gray-800 border border-gray-150 dark:border-gray-775 hover:border-gray-300 dark:hover:border-gray-725"
+    className="group relative px-4 py-3 cursor-pointer overflow-hidden rounded-lg shadow-lg bg-white dark:bg-gray-800 border border-gray-150 dark:border-gray-775 hover:border-gray-300 dark:hover:border-gray-725"
     title="All time"
     onClick={onClick}
   >
     <dt>
-      <p className="pb-0.5 text-sm font-medium text-gray-500 truncate">{name}</p>
+      <div className="flex items-center">
+        <p className="pb-0.5 text-sm font-medium text-gray-500 group-hover:dark:text-gray-475 group-hover:text-gray-600 truncate">{name}</p>
+        <LinkIcon className="h-3 w-3 ml-2 text-gray-500 group-hover:dark:text-gray-475 group-hover:text-gray-600" aria-hidden="true" />
+      </div>
     </dt>
 
     <dd className="flex items-baseline">
@@ -57,7 +61,7 @@ export const Stats = () => {
         Stats
       </h1>
 
-      <dl className={classNames("grid grid-cols-1 gap-5 mt-5 sm:grid-cols-2 lg:grid-cols-4", isLoading ? "animate-pulse" : "")}>
+      <dl className={classNames("grid grid-cols-2 gap-5 mt-5 sm:grid-cols-2 lg:grid-cols-4", isLoading ? "animate-pulse" : "")}>
         <StatsItem name="Filtered Releases" onClick={() => handleStatClick("")} value={data?.filtered_count ?? 0} />
         {/* <StatsItem name="Filter Rejected Releases" stat={data?.filter_rejected_count} /> */}
         <StatsItem name="Approved Pushes" onClick={() => handleStatClick("PUSH_APPROVED")}  value={data?.push_approved_count ?? 0} />
