d21631caa37586ea3839d90789f32e98384686db
ze0s <43699394+zze0s@users.noreply.github.com>
2023-12-25 13:21:53 +0100

    fix(http): log msg

--- internal/http/server.go
+++ internal/http/server.go
@@ -82,7 +82,7 @@ func (s Server) Open() error {
 			break
 		}
 
-		s.log.Err(err).Msgf("Failed to start %s server. Attempted to listen on %s", proto, addr)
+		s.log.Error().Err(err).Msgf("Failed to start %s server. Attempted to listen on %s", proto, addr)
 	}
 
 	return err
