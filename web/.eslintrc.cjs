907d426d82ed12a1321aa60e117a24991831849c
martylukyy <35452459+martylukyy@users.noreply.github.com>
2023-12-27 16:01:57 +0100

    add eslint rules

--- web/.eslintrc.cjs
+++ web/.eslintrc.cjs
@@ -8,11 +8,16 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: [
+    '@stylistic',
+    'react-refresh'
+  ],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    '@stylistic/comma-dangle': ["error", "never"],
+    '@stylistic/indent': ['error', 2, { "SwitchCase": 1 }],
+    '@stylistic/object-curly-spacing': ["error", "always"],
+    '@typescript-eslint/no-unused-vars': ["warn"],
+    'linebreak-style': ["error", "unix"],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }]
   },
 }
