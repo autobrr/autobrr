9a984fc6ef09a7ea3ead2651db9f6e7d1a6ceb42
martylukyy <35452459+martylukyy@users.noreply.github.com>
2023-12-31 11:18:16 +0100

    chore(lint): configure double quotes for strings substitute single quotes with double quotes in .eslintrc.cjs

--- web/.eslintrc.cjs
+++ web/.eslintrc.cjs
@@ -2,22 +2,23 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
   plugins: [
-    '@stylistic',
-    'react-refresh'
+    "@stylistic",
+    "react-refresh"
   ],
   rules: {
-    '@stylistic/comma-dangle': ["error", "never"],
-    '@stylistic/indent': ['error', 2, { "SwitchCase": 1 }],
-    '@stylistic/object-curly-spacing': ["error", "always"],
-    '@typescript-eslint/no-unused-vars': ["warn"],
-    'linebreak-style': ["error", "unix"],
-    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }]
+    "@stylistic/comma-dangle": ["error", "never"],
+    "@stylistic/indent": ["error", 2, { "SwitchCase": 1 }],
+    "@stylistic/object-curly-spacing": ["error", "always"],
+    "@typescript-eslint/no-unused-vars": ["warn"],
+    "linebreak-style": ["error", "unix"],
+    "quotes": ["error", "double"],
+    "react-refresh/only-export-components": ["warn", { allowConstantExport: true }]
   },
 }
