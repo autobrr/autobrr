c22aa5170044b70ee954dc3b8704fdbac5b2ae66
Brandon Beck <bmbeck@gmail.com>
2024-02-19 18:36:14 -0600

    feat(macros): add FilterID

--- internal/domain/macros_test.go
+++ internal/domain/macros_test.go
@@ -236,6 +236,15 @@ func TestMacros_Parse(t *testing.T) {
 			want:    "Type: episode",
 			wantErr: false,
 		},
+		{
+			name: "test_filter_id",
+			release: Release{
+				FilterID: 1,
+			},
+			args:    args{text: "FilterID: {{ .FilterID }}"},
+			want:    "FilterID: 1",
+			wantErr: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
