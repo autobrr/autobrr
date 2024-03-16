9f806c88305111da198e378cbefed05bf5455131
Kyle Sanderson <kyle.leet@gmail.com>
2024-03-16 14:52:30 -0700

    i

--- internal/feed/rss_test.go
+++ internal/feed/rss_test.go
@@ -272,7 +272,7 @@ func Test_readSizeFromDescription(t *testing.T) {
 		{
 			name: "upgrade size",
 			str:  `<p> <strong>Name</strong>: One.S01E01.German.DL.DTS.1080p.BluRay.x265.10bit-Cats<br> <strong>Category</strong>: Anime Serien<br> <strong>Type</strong>: Encode<br> <strong>Resolution</strong>: 1080p<br> <strong>Size</strong>: 2.49 GiB<br> <strong>Uploaded</strong>: vor 3 Minuten<br> <strong>Seeders</strong>: 1 | <strong>Leechers</strong>: 7 | <strong>Completed</strong>: 0<br> <strong>Uploader</strong>: Hochgeladen von xxx <br> IMDB Link:<a href="https://anon.to?http://www.imdb.com/title/tt1" target="_blank">tt1</a><br> TMDB Link: <a href="https://anon.to?https://www.themoviedb.org/tv/1" target="_blank">1</a><br> </p>`,
-			want: "2.49GB",
+			want: "2.49GiB",
 		},
 	}
 
