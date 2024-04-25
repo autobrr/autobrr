b4fdeceb85f8f5c92c898657a2296869f9f1c946
s0up4200 <s0up4200@pm.me>
2024-04-25 23:20:18 +0200

    add AgeSelect component

--- web/src/screens/settings/Releases.tsx
+++ web/src/screens/settings/Releases.tsx
@@ -7,6 +7,7 @@ import { useRef, useState } from "react";
 import { useMutation, useQueryClient, useQuery } from "@tanstack/react-query";
 import { toast } from "react-hot-toast";
 import { MultiSelect as RMSC } from "react-multi-select-component";
+import { AgeSelect } from "@components/inputs"
 
 import { APIClient } from "@api/APIClient";
 import { ReleaseKeys } from "@api/query_keys";
@@ -83,9 +84,9 @@ function DeleteReleases() {
   });
 
   const releaseStatusOptions = [
-    { label: "PUSH_APPROVED", value: "PUSH_APPROVED" },
-    { label: "PUSH_REJECTED", value: "PUSH_REJECTED" },
-    { label: "PUSH_ERROR", value: "PUSH_ERROR" }
+    { label: "Approved", value: "PUSH_APPROVED" },
+    { label: "Rejected", value: "PUSH_REJECTED" },
+    { label: "Errored", value: "PUSH_ERROR" }
   ];
 
   const deleteOlderMutation = useMutation({
@@ -133,33 +134,16 @@ function DeleteReleases() {
         </div>
         <div className="flex flex-row gap-2 py-4 items-center">
           <div className="flex w-full flex-col">
-            <p className="text-sm text-gray-500 dark:text-gray-400 p-1">Select age:</p>
-            <select
-              name="duration"
-              id="duration"
-              className="w-full focus:outline-none focus:ring-1 focus:ring-offset-0 focus:ring-blue-500 dark:focus:ring-blue-500 rounded-md sm:text-sm border-gray-300 dark:border-gray-600 dark:bg-gray-800 dark:text-white"
-              value={duration}
-              onChange={(e) => {
-                const parsedDuration = parseInt(e.target.value, 10);
-                setParsedDuration(parsedDuration);
-                setDuration(e.target.value);
-              }}
-            >
-              <option value="">Select...</option>
-              <option value="1">1 hour</option>
-              <option value="12">12 hours</option>
-              <option value="24">1 day</option>
-              <option value="168">1 week</option>
-              <option value="720">1 month</option>
-              <option value="2160">3 months</option>
-              <option value="4320">6 months</option>
-              <option value="8760">1 year</option>
-              <option value="0">Delete everything</option>
-            </select>
+            <p className="text-sm text-gray-900 dark:text-gray-400 p-1">Select age:</p>
+            <AgeSelect
+              duration={duration}
+              setDuration={setDuration}
+              setParsedDuration={setParsedDuration}
+            />
           </div>
 
           <div className="flex w-full flex-col">
-            <p className="text-sm text-gray-500 dark:text-gray-400 p-1">Indexers:</p>
+            <p className="text-sm text-gray-900 dark:text-gray-400 p-1">Indexers:</p>
             <RMSC
               options={indexerOptions?.map(option => ({ value: option.identifier, label: option.name })) || []}
               value={indexers}
@@ -170,7 +154,7 @@ function DeleteReleases() {
           </div>
 
           <div className="flex w-full flex-col">
-            <p className="text-sm text-gray-500 dark:text-gray-400 p-1">Release status:</p>
+            <p className="text-sm text-gray-900 dark:text-gray-400 p-1">Release status:</p>
             <RMSC
               options={releaseStatusOptions}
               value={releaseStatuses}
@@ -183,10 +167,11 @@ function DeleteReleases() {
           <button
             type="button"
             onClick={toggleDeleteModal}
-            className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-red-700 hover:text-red-800 dark:text-white bg-red-200 dark:bg-red-700 hover:bg-red-300 dark:hover:bg-red-800 focus:outline-none focus:ring-1 focus:ring-inset focus:ring-red-600"
+            className="inline-flex items-center px-4 py-2 mt-7 border border-transparent text-sm font-medium rounded-md text-red-700 hover:text-red-800 dark:text-white bg-red-200 dark:bg-red-700 hover:bg-red-300 dark:hover:bg-red-800 focus:outline-none focus:ring-1 focus:ring-inset focus:ring-red-600"
           >
             Delete
           </button>
+
         </div>
       </div>
     </div>
