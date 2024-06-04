31d7371007415a847496ceaf76c2b023ad0dec95
martylukyy <35452459+martylukyy@users.noreply.github.com>
2024-06-04 14:27:47 +0200

    refactor(web): fix build errors

--- web/src/components/modals/index.tsx
+++ web/src/components/modals/index.tsx
@@ -5,7 +5,7 @@
 
 
 import { FC, Fragment, MutableRefObject, useState } from "react";
-import { Dialog, Transition } from "@headlessui/react";
+import { Dialog, DialogPanel, DialogTitle, Transition, TransitionChild } from "@headlessui/react";
 import { ExclamationTriangleIcon } from "@heroicons/react/24/solid";
 
 import { RingResizeSpinner } from "@components/Icons";
@@ -42,9 +42,9 @@ const ModalUpper = ({ title, text }: ModalUpperProps) => (
     <div className="sm:flex sm:items-start">
       <ExclamationTriangleIcon className="h-16 w-16 text-red-500 dark:text-red-500" aria-hidden="true" />
       <div className="mt-3 text-left sm:mt-0 sm:ml-4 sm:pr-8 max-w-full">
-        <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-gray-900 dark:text-white break-words">
+        <DialogTitle as="h3" className="text-lg leading-6 font-medium text-gray-900 dark:text-white break-words">
           {title}
-        </Dialog.Title>
+        </DialogTitle>
         <div className="mt-2">
           <p className="text-sm text-gray-500 dark:text-gray-300">
             {text}
@@ -90,7 +90,7 @@ const ModalLower = ({ isOpen, isLoading, toggle, deleteAction }: ModalLowerProps
 );
 
 export const DeleteModal: FC<DeleteModalProps> = (props: DeleteModalProps) => (
-  <Transition.Root show={props.isOpen} as={Fragment}>
+  <Transition show={props.isOpen} as={Fragment}>
     <Dialog
       as="div"
       static
@@ -100,7 +100,7 @@ export const DeleteModal: FC<DeleteModalProps> = (props: DeleteModalProps) => (
       onClose={props.toggle}
     >
       <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-        <Transition.Child
+        <TransitionChild
           as={Fragment}
           enter="ease-out duration-300"
           enterFrom="opacity-0"
@@ -109,13 +109,13 @@ export const DeleteModal: FC<DeleteModalProps> = (props: DeleteModalProps) => (
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
-          <Dialog.Overlay className="fixed inset-0 bg-gray-700/60 dark:bg-black/60 transition-opacity" />
-        </Transition.Child>
+          <DialogPanel className="fixed inset-0 bg-gray-700/60 dark:bg-black/60 transition-opacity" />
+        </TransitionChild>
 
         <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
           &#8203;
         </span>
-        <Transition.Child
+        <TransitionChild
           as={Fragment}
           enter="ease-out duration-300"
           enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
@@ -128,10 +128,10 @@ export const DeleteModal: FC<DeleteModalProps> = (props: DeleteModalProps) => (
             <ModalUpper {...props} />
             <ModalLower {...props} />
           </div>
-        </Transition.Child>
+        </TransitionChild>
       </div>
     </Dialog>
-  </Transition.Root>
+  </Transition>
 );
 
 export const ForceRunModal: FC<ForceRunModalProps> = (props: ForceRunModalProps) => {
@@ -170,7 +170,7 @@ export const ForceRunModal: FC<ForceRunModalProps> = (props: ForceRunModalProps)
   };
 
   return (
-    <Transition.Root show={props.isOpen} as={Fragment}>
+    <Transition show={props.isOpen} as={Fragment}>
       <Dialog
         as="div"
         static
@@ -180,7 +180,7 @@ export const ForceRunModal: FC<ForceRunModalProps> = (props: ForceRunModalProps)
         onClose={handleClose}
       >
         <div className="grid place-items-center min-h-screen">
-          <Transition.Child
+          <TransitionChild
             as={Fragment}
             enter="ease-out duration-300"
             enterFrom="opacity-0"
@@ -188,11 +188,9 @@ export const ForceRunModal: FC<ForceRunModalProps> = (props: ForceRunModalProps)
             leave="ease-in duration-200"
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
-          >
-            <Dialog.Overlay className="fixed inset-0 bg-gray-700/60 dark:bg-black/60 transition-opacity" />
-          </Transition.Child>
+          />
 
-          <Transition.Child
+          <TransitionChild
             as={Fragment}
             enter="ease-out duration-300"
             enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
@@ -201,7 +199,7 @@ export const ForceRunModal: FC<ForceRunModalProps> = (props: ForceRunModalProps)
             leaveFrom="opacity-100 translate-y-0 sm:scale-100"
             leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
           >
-            <div className="inline-block align-bottom border border-transparent dark:border-gray-700 rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+            <DialogPanel className="inline-block align-bottom border border-transparent dark:border-gray-700 rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
               <ModalUpper title={props.title} text={props.text} />
               
               <div className="bg-gray-50 dark:bg-gray-800 px-4 py-3 sm:px-6 flex justify-center">
@@ -244,10 +242,10 @@ export const ForceRunModal: FC<ForceRunModalProps> = (props: ForceRunModalProps)
                   </>
                 )}
               </div>
-            </div>
-          </Transition.Child>
+            </DialogPanel>
+          </TransitionChild>
         </div>
       </Dialog>
-    </Transition.Root>
+    </Transition>
   );
 };
