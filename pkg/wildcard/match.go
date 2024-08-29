3bf55eb7e6267f221fe6a7c76539f63c3d1211ef
Kyle Sanderson <kyle.leet@gmail.com>
2024-08-29 01:41:28 -0700

    fix(wildcard): avoid excessive allocations every loop

--- pkg/wildcard/match.go
+++ pkg/wildcard/match.go
@@ -33,22 +33,57 @@ func Match(pattern, name string) (matched bool) {
 }
 
 func deepMatchRune(str, pattern []rune, simple bool) bool {
-	for len(pattern) > 0 {
-		switch pattern[0] {
-		default:
-			if len(str) == 0 || str[0] != pattern[0] {
+	if len(str) < len(pattern) {
+		return false
+	}
+
+	k := 0
+	for i := 0; i < len(pattern); i++ {
+		switch pattern[i] {
+		case '*':
+			if i == len(pattern)-1 {
+				return true
+			}
+
+			var val rune
+			for ; i < len(pattern); i++ {
+				val = pattern[i+1]
+				if !simple && val == '?' || val == '*' {
+					continue
+				}
+
+				break
+			}
+
+			if i == len(pattern)-1 {
+				return true
+			}
+
+			for ; k < len(str); k++ {
+				if str[k] != val {
+					continue
+				}
+
+				break
+			}
+
+			if k == len(str) {
 				return false
 			}
 		case '?':
-			if len(str) == 0 && !simple {
+			if simple && pattern[i] != str[k] {
 				return false
 			}
-		case '*':
-			return deepMatchRune(str, pattern[1:], simple) ||
-				(len(str) > 0 && deepMatchRune(str[1:], pattern, simple))
+
+			k++
+			continue
+		case str[k]:
+			k++
+			continue
+		default:
+			return false
 		}
-		str = str[1:]
-		pattern = pattern[1:]
 	}
-	return len(str) == 0 && len(pattern) == 0
+
+	return str[len(str)-1] == pattern[len(pattern)-1]
 }
