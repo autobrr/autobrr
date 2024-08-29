34d6e0cf609e225fa22261177d4b43c258f03566
ze0s <43699394+zze0s@users.noreply.github.com>
2024-08-29 13:21:01 +0200

    fix(diagnostics): profiling config flags (#1637)

--- internal/domain/config.go
+++ internal/domain/config.go
@@ -24,7 +24,7 @@ type Config struct {
 	PostgresPass        string `toml:"postgresPass"`
 	PostgresSSLMode     string `toml:"postgresSSLMode"`
 	PostgresExtraParams string `toml:"postgresExtraParams"`
-	ProfilingEnabled    bool   `toml:"profiling"`
+	ProfilingEnabled    bool   `toml:"profilingEnabled"`
 	ProfilingHost       string `toml:"profilingHost"`
 	ProfilingPort       int    `toml:"profilingPort"`
 }
