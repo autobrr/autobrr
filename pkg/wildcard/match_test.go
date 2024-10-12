5845be18ee102292747368fdb129426f509112e7
Kyle Sanderson <kyle.leet@gmail.com>
2024-10-12 21:07:31 +0800

    tests(wildcard): ?

--- pkg/wildcard/match_test.go
+++ pkg/wildcard/match_test.go
@@ -107,6 +107,11 @@ func TestMatch(t *testing.T) {
 			text:    "them",
 			matched: false,
 		},
+		{
+			pattern: "t?q*",
+			text:    "tam e",
+			matched: false,
+		},
 	}
 	// Iterating over the test cases, call the function under test and assert the output.
 	for i, testCase := range testCases {
