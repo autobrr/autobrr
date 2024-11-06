a186eaf299e05f1545d23afc236f047064ed03c9
ze0s <ze0s@riseup.net>
2024-11-06 15:10:24 +0100

    fix(proxy): use separate transport for proxies

--- pkg/sharedhttp/http.go
+++ pkg/sharedhttp/http.go
@@ -52,6 +52,26 @@ var TransportTLSInsecure = &http.Transport{
 	},
 }
 
+var ProxyTransport = &http.Transport{
+	Proxy: http.ProxyFromEnvironment,
+	DialContext: (&net.Dialer{
+		Timeout:   30 * time.Second, // default transport value
+		KeepAlive: 30 * time.Second, // default transport value
+	}).DialContext,
+	ForceAttemptHTTP2:     true,              // default is true; since HTTP/2 multiplexes a single TCP connection.
+	MaxIdleConns:          100,               // default transport value
+	MaxIdleConnsPerHost:   10,                // default is 2, so we want to increase the number to use establish more connections.
+	IdleConnTimeout:       90 * time.Second,  // default transport value
+	ResponseHeaderTimeout: 120 * time.Second, // servers can respond slowly - this should fix some portion of releases getting stuck as pending.
+	TLSHandshakeTimeout:   10 * time.Second,  // default transport value
+	ExpectContinueTimeout: 1 * time.Second,   // default transport value
+	ReadBufferSize:        65536,
+	WriteBufferSize:       65536,
+	TLSClientConfig: &tls.Config{
+		MinVersion: tls.VersionTLS12,
+	},
+}
+
 var Client = &http.Client{
 	Timeout:   60 * time.Second,
 	Transport: Transport,
