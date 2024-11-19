194f0b1f562ea4a4c46ac865c7c204b2ebeadb53
Kyle Sanderson <kyle.leet@gmail.com>
2024-11-19 10:56:19 -0800

    more rounds

--- pkg/timecache/timecache_test.go
+++ pkg/timecache/timecache_test.go
@@ -21,3 +21,29 @@ func TestRounding(t *testing.T) {
 		t.Fatalf("time is not a 5 multiple")
 	}
 }
+
+func TestResolution(t *testing.T) {
+	t.Parallel()
+	const magicNumber = 3
+	const rounds = 700
+	ti := New(Options{}.Round(time.Millisecond * magicNumber))
+
+	unique := 0
+	old := ti.Now().UnixMilli()
+	for i := 0; i < rounds; i++ {
+		new := ti.Now().UnixMilli()
+		if new > old {
+			unique++
+			old = new
+		}
+
+		if div := new % magicNumber; div != 0 {
+			t.Fatalf("not a multiple of %d: %d", magicNumber, div)
+		}
+		time.Sleep(time.Millisecond * 1)
+	}
+
+	if unique < rounds/magicNumber-1 {
+		t.Fatalf("not enough resolution rounds %d", unique)
+	}
+}
