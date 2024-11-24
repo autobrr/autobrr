f3ecd3bfca1d33aa56c2a29ad68e680b6a34f1bc
ze0s <ze0s@riseup.net>
2024-11-24 16:22:20 +0100

    feat(indexers): update SeedPool IRC port

--- internal/domain/release.go
+++ internal/domain/release.go
@@ -683,7 +683,7 @@ func (r *Release) MapVars(def *IndexerDefinition, varMap map[string]string) erro
 	}
 
 	if freeleech, err := getStringMapValue(varMap, "freeleech"); err == nil {
-		fl := StringEqualFoldMulti(freeleech, "1", "free", "freeleech", "freeleech!", "yes", "VIP")
+		fl := StringEqualFoldMulti(freeleech, "1", "free", "freeleech", "freeleech!", "yes", "VIP", "★")
 		if fl {
 			r.Freeleech = true
 			// default to 100 and override if freeleechPercent is present in next function
