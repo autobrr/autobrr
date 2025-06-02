8e27fb20ff0d2af1911eba3a94a1b9e2e81c87e9
ze0s <ze0s@riseup.net>
2025-06-02 18:22:44 +0200

    feat(feeds): add db integration test for ExistingItems

--- internal/database/feed_cache_test.go
+++ internal/database/feed_cache_test.go
@@ -155,6 +155,107 @@ func TestFeedCacheRepo_Exists(t *testing.T) {
 	}
 }
 
+func TestFeedCacheRepo_ExistingItems(t *testing.T) {
+	for dbType, db := range testDBs {
+
+		log := setupLoggerForTest()
+		repo := NewFeedCacheRepo(log, db)
+		feedRepo := NewFeedRepo(log, db)
+		indexerRepo := NewIndexerRepo(log, db)
+		mockData := getMockFeed()
+		indexerMockData := getMockIndexer()
+
+		t.Run(fmt.Sprintf("ExistingItems_SingleItem_Multi_Keys [%s]", dbType), func(t *testing.T) {
+			// Setup
+			indexer, err := indexerRepo.Store(t.Context(), indexerMockData)
+			assert.NoError(t, err)
+			mockData.IndexerID = int(indexer.ID)
+
+			err = feedRepo.Store(t.Context(), mockData)
+			assert.NoError(t, err)
+
+			err = repo.Put(mockData.ID, "test_key", []byte("test_value"), time.Now().Add(time.Hour))
+			assert.NoError(t, err)
+
+			keys := []string{"test_key", "test_key_2"}
+
+			// Execute
+			items, err := repo.ExistingItems(t.Context(), mockData.ID, keys)
+			assert.NoError(t, err)
+			assert.Len(t, items, 1)
+			//assert.True(t, exists)
+
+			// Cleanup
+			_ = feedRepo.Delete(t.Context(), mockData.ID)
+			_ = indexerRepo.Delete(t.Context(), int(indexer.ID))
+			_ = repo.Delete(t.Context(), mockData.ID, "test_key")
+		})
+
+		t.Run(fmt.Sprintf("ExistingItems_MultipleItems [%s]", dbType), func(t *testing.T) {
+			// Setup
+			indexer, err := indexerRepo.Store(t.Context(), indexerMockData)
+			assert.NoError(t, err)
+			mockData.IndexerID = int(indexer.ID)
+
+			err = feedRepo.Store(t.Context(), mockData)
+			assert.NoError(t, err)
+
+			err = repo.Put(mockData.ID, "test_key", []byte("test_value"), time.Now().Add(time.Hour))
+			assert.NoError(t, err)
+
+			err = repo.Put(mockData.ID, "test_key_2", []byte("test_value_2"), time.Now().Add(time.Hour))
+			assert.NoError(t, err)
+
+			keys := []string{"test_key", "test_key_2"}
+
+			// Execute
+			items, err := repo.ExistingItems(t.Context(), mockData.ID, keys)
+			assert.NoError(t, err)
+			assert.Len(t, items, 2)
+
+			// Cleanup
+			_ = feedRepo.Delete(t.Context(), mockData.ID)
+			_ = indexerRepo.Delete(t.Context(), int(indexer.ID))
+			_ = repo.Delete(t.Context(), mockData.ID, "test_key")
+		})
+
+		t.Run(fmt.Sprintf("ExistingItems_MultipleItems_Single_Key [%s]", dbType), func(t *testing.T) {
+			// Setup
+			indexer, err := indexerRepo.Store(t.Context(), indexerMockData)
+			assert.NoError(t, err)
+			mockData.IndexerID = int(indexer.ID)
+
+			err = feedRepo.Store(t.Context(), mockData)
+			assert.NoError(t, err)
+
+			err = repo.Put(mockData.ID, "test_key", []byte("test_value"), time.Now().Add(time.Hour))
+			assert.NoError(t, err)
+
+			err = repo.Put(mockData.ID, "test_key_2", []byte("test_value_2"), time.Now().Add(time.Hour))
+			assert.NoError(t, err)
+
+			keys := []string{"test_key"}
+
+			// Execute
+			items, err := repo.ExistingItems(t.Context(), mockData.ID, keys)
+			assert.NoError(t, err)
+			assert.Len(t, items, 2)
+
+			// Cleanup
+			_ = feedRepo.Delete(t.Context(), mockData.ID)
+			_ = indexerRepo.Delete(t.Context(), int(indexer.ID))
+			_ = repo.Delete(t.Context(), mockData.ID, "test_key")
+		})
+
+		t.Run(fmt.Sprintf("ExistsItems_Nonexistent_Key [%s]", dbType), func(t *testing.T) {
+			// Execute
+			exists, err := repo.Exists(-1, "nonexistent_key")
+			assert.NoError(t, err)
+			assert.False(t, exists)
+		})
+	}
+}
+
 func TestFeedCacheRepo_Put(t *testing.T) {
 	for dbType, db := range testDBs {
 		log := setupLoggerForTest()
