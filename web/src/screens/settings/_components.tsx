7a7e835026fb7797071b514e2e42832a7280a7d7
ze0s <ze0s@riseup.net>
2026-01-26 23:36:59 +0100

    fix(notifications): better webhook icon

--- web/src/screens/settings/_components.tsx
+++ web/src/screens/settings/_components.tsx
@@ -145,19 +145,18 @@ export const LunaSeaIcon = () => (
 );
 
 export const WebhookIcon = () => (
-  <svg {...commonSVGProps} viewBox="0 0 24 24">
-    {/* Bottom left node */}
-    <circle cx="5" cy="18" r="3" />
-    {/* Top center node */}
-    <circle cx="12" cy="5" r="3" />
-    {/* Bottom right node */}
-    <circle cx="19" cy="18" r="3" />
-    {/* Line from top to center hub */}
-    <path d="M12 8v4" strokeWidth="2" stroke="currentColor" />
-    {/* Line from center to bottom left */}
-    <path d="M12 12L6.5 16.5" strokeWidth="2" stroke="currentColor" />
-    {/* Line from center to bottom right */}
-    <path d="M12 12L17.5 16.5" strokeWidth="2" stroke="currentColor" />
+  <svg
+    {...commonSVGProps}
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <path d="M18 16.98h-5.99c-1.1 0-1.95.94-2.48 1.9A4 4 0 0 1 2 17c.01-.7.2-1.4.57-2" />
+    <path d="m6 17 3.13-5.78c.53-.97.1-2.18-.5-3.1a4 4 0 1 1 6.89-4.06" />
+    <path d="m12 6 3.13 5.73C15.66 12.7 16.9 13 18 13a4 4 0 0 1 0 8" />
   </svg>
 )
 
