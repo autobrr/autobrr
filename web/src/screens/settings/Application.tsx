19bb7b32674df0a651fa8783a0df99e5b0458f57
NoeR <112766774+NoeRi9n@users.noreply.github.com>
2026-04-05 22:42:42 +0530

    Update Application.tsx

--- web/src/screens/settings/Application.tsx
+++ web/src/screens/settings/Application.tsx
@@ -191,6 +191,7 @@ function ApplicationSettings() {
           >
             <option value="en">{t("common:language.english")}</option>
             <option value="zh-CN">{t("common:language.simplifiedChinese")}</option>
+            <option value="ru">{t("common:language.russian")}</option>
           </select>
           </div>
         </div>
