d7823283f5445c808664d5a1cb2a03966aca14b3
ze0s <43699394+zze0s@users.noreply.github.com>
2026-04-06 16:17:02 +0200

    Update DownloadURL assignment for magnet feeds

--- internal/feed/rss.go
+++ internal/feed/rss.go
@@ -163,7 +163,7 @@ func (j *RSSJob) processItem(item *gofeed.Item) *domain.Release {
 			if j.Feed.Settings != nil && j.Feed.Settings.DownloadType == domain.FeedDownloadTypeMagnet {
 				if !strings.HasPrefix(rls.MagnetURI, domain.MagnetURIPrefix) && strings.HasPrefix(e.URL, domain.MagnetURIPrefix) {
 					rls.MagnetURI = e.URL
-					rls.DownloadURL = ""
+					rls.DownloadURL = e.URL
 				}
 			}
 			break
