aa355c43fca304435daf7062d76af7d42c5b5da3
ze0s <ze0s@riseup.net>
2026-08-05 21:22:10 +0200

    fix(database): migrations order

--- internal/database/migrations/postgres.go
+++ internal/database/migrations/postgres.go
@@ -105,7 +105,7 @@ func PostgresMigrations(db *sql.DB, logger zerolog.Logger) *migrator.Migrator {
 	migrate.AddFileMigration("82_indexers_rename_rotorrent_to_seedcore.sql")
 	migrate.AddFileMigration("83_create_release_status_indexes.sql")
 	migrate.AddFileMigration("84_feeds_add_user_agent.sql")
-  migrate.AddFileMigration("83_add_action_status_retry_and_position.sql")
+	migrate.AddFileMigration("85_add_action_status_retry_and_position.sql")
 
 	return migrate
 }
