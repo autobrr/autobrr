autobrr/autobrr

commit 0a3c763f71da5cfb693650509d786b3bc0de27f3

refactor(downloader): unify downloader service and list processors (#2650)
* refactor(downloader): client cache

* refactor(downloader): rename download client to downloader

* fix: codeql review comments

internal/action/service.go
@@ -9,7 +9,9 @@ import (
 	"time"
 
 	"github.com/autobrr/autobrr/internal/domain"
+	"github.com/autobrr/autobrr/internal/downloader"
 	"github.com/autobrr/autobrr/internal/events"
+	"github.com/autobrr/autobrr/pkg/errors"
 	"github.com/autobrr/autobrr/pkg/sharedhttp"
 
 	"github.com/rs/zerolog"
@@ -27,12 +29,12 @@ type actionRepo interface {
 	ToggleEnabled(actionID int) error
 }
 
-type clientService interface {
-	FindByID(ctx context.Context, id int32) (*domain.DownloadClient, error)
-	GetClient(ctx context.Context, clientId int32) (*domain.DownloadClient, error)
+type downloaderService interface {
+	FindByID(ctx context.Context, id int32) (*domain.Downloader, error)
+	GetInstance(ctx context.Context, clientId int32) (*downloader.Instance, error)
 }
 
-type downloadService interface {
+type rlsDownloadService interface {
 	DownloadRelease(ctx context.Context, rls *domain.Release) error
 	ResolveMagnetURI(ctx context.Context, r *domain.Release) error
 }
@@ -42,22 +44,22 @@ type eventBus interface {
 }
 
 type Service struct {
-	log         zerolog.Logger
-	eventBus    eventBus
-	repo        actionRepo
-	clientSvc   clientService
-	downloadSvc downloadService
+	log            zerolog.Logger
+	eventBus       eventBus
+	repo           actionRepo
+	downloaderSvc  downloaderService
+	rlsDownloadSvc rlsDownloadService
 
 	httpClient *http.Client
 }
 
-func NewService(log zerolog.Logger, bus eventBus, repo actionRepo, clientSvc clientService, downloadSvc downloadService) *Service {
+func NewService(log zerolog.Logger, bus eventBus, repo actionRepo, clientSvc downloaderService, downloadSvc rlsDownloadService) *Service {
 	s := &Service{
-		log:         log.With().Str("module", "action").Logger(),
-		eventBus:    bus,
-		repo:        repo,
-		clientSvc:   clientSvc,
-		downloadSvc: downloadSvc,
+		log:            log.With().Str("module", "action").Logger(),
+		eventBus:       bus,
+		repo:           repo,
+		downloaderSvc:  clientSvc,
+		rlsDownloadSvc: downloadSvc,
 
 		httpClient: &http.Client{
 			Timeout:   time.Second * 120,
@@ -92,7 +94,7 @@ func (s *Service) Get(ctx context.Context, req *domain.GetActionRequest) (*domai
 
 	// optionally attach download client to action
 	if a.ClientID > 0 {
-		client, err := s.clientSvc.FindByID(ctx, a.ClientID)
+		client, err := s.downloaderSvc.FindByID(ctx, a.ClientID)
 		if err != nil {
 			return nil, err
 		}
@@ -117,4 +119,27 @@ func (s *Service) DeleteByFilterID(ctx context.Context, filterID int) error {
 
 func (s *Service) ToggleEnabled(actionID int) error {
 	return s.repo.ToggleEnabled(actionID)
+}
+
+func (s *Service) getClientInstance[T any](ctx context.Context, clientID int32) (*T, error) {
+	instance, err := s.downloaderSvc.GetInstance(ctx, clientID)
+	if err != nil {
+		return nil, err
+	}
+
+	cfg := instance.Config()
+	if cfg == nil {
+		return nil, errors.New("client %d has no config", clientID)
+	}
+
+	if !cfg.Enabled {
+		return nil, errors.New("client %s %s not enabled", cfg.Type, cfg.Name)
+	}
+
+	client, err := downloader.ClientAs[T](instance)
+	if err != nil {
+		return nil, err
+	}
+
+	return &client, nil
 }

internal/list/processor.go
@@ -1,0 +1,177 @@
+package list
+
+import (
+	"context"
+	"time"
+
+	"github.com/autobrr/autobrr/internal/domain"
+	"github.com/autobrr/autobrr/internal/downloader"
+	"github.com/autobrr/autobrr/pkg/arr/lidarr"
+	"github.com/autobrr/autobrr/pkg/arr/radarr"
+	"github.com/autobrr/autobrr/pkg/arr/readarr"
+	"github.com/autobrr/autobrr/pkg/arr/sonarr"
+	"github.com/autobrr/autobrr/pkg/arr/sportarr"
+	"github.com/autobrr/autobrr/pkg/arr/whisparr"
+
+	"github.com/pkg/errors"
+	"github.com/rs/zerolog"
+)
+
+type Processor interface {
+	Process(ctx context.Context) (*domain.FilterUpdate, error)
+}
+
+type processorBase struct {
+	log  zerolog.Logger
+	list *domain.List
+}
+
+func (s *Service) getClientInstance[T any](ctx context.Context, clientID int) (*T, error) {
+	instance, err := s.downloaderSvc.GetInstance(ctx, int32(clientID))
+	if err != nil {
+		return nil, err
+	}
+
+	cfg := instance.Config()
+	if cfg == nil {
+		return nil, errors.Errorf("client %d has no config", clientID)
+	}
+
+	if !cfg.Enabled {
+		return nil, errors.Errorf("client %s %s not enabled", cfg.Type, cfg.Name)
+	}
+
+	client, err := downloader.ClientAs[T](instance)
+	if err != nil {
+		return nil, err
+	}
+
+	return &client, nil
+}
+
+func (s *Service) getProcessor(ctx context.Context, list *domain.List) (Processor, error) {
+	var processor Processor
+	switch list.Type {
+	case domain.ListTypeLidarr:
+		client, err := s.getClientInstance[lidarr.Client](ctx, list.ClientID)
+		if err != nil {
+			return nil, err
+		}
+		processor = NewLidarrProcessor(s.log, list, client)
+
+	case domain.ListTypeRadarr:
+		client, err := s.getClientInstance[radarr.Client](ctx, list.ClientID)
+		if err != nil {
+			return nil, err
+		}
+		processor = NewRadarrProcessor(s.log, list, client)
+
+	case domain.ListTypeReadarr:
+		client, err := s.getClientInstance[readarr.Client](ctx, list.ClientID)
+		if err != nil {
+			return nil, err
+		}
+		processor = NewReadarrProcessor(s.log, list, client)
+
+	case domain.ListTypeSonarr:
+		client, err := s.getClientInstance[sonarr.Client](ctx, list.ClientID)
+		if err != nil {
+			return nil, err
+		}
+		processor = NewSonarrProcessor(s.log, list, client)
+
+	case domain.ListTypeSportarr:
+		client, err := s.getClientInstance[sportarr.Client](ctx, list.ClientID)
+		if err != nil {
+			return nil, err
+		}
+		processor = NewSportarrProcessor(s.log, list, client)
+
+	case domain.ListTypeWhisparr, domain.ListTypeWhisparrV3:
+		client, err := s.getClientInstance[whisparr.Client](ctx, list.ClientID)
+		if err != nil {
+			return nil, err
+		}
+		processor = NewWhisparrProcessor(s.log, list, client)
+
+	case domain.ListTypeAniList:
+		processor = NewAnilistProcessor(s.log, list)
+	case domain.ListTypeMDBList:
+		processor = NewMDBListProcessor(s.log, list)
+	case domain.ListTypeMetacritic:
+		processor = NewMetacriticProcessor(s.log, list)
+	case domain.ListTypePlaintext:
+		processor = NewPlaintextProcessor(s.log, list)
+	case domain.ListTypeSteam:
+		processor = NewSteamProcessor(s.log, list)
+	case domain.ListTypeTrakt:
+		processor = NewTraktProcessor(s.log, list)
+	default:
+		return nil, errors.Errorf("unknown list type: %s", list.Type)
+	}
+
+	return processor, nil
+}
+
+func (s *Service) Process(ctx context.Context, list *domain.List) error {
+	processor, err := s.getProcessor(ctx, list)
+	if err != nil {
+		return err
+	}
+
+	if err := s.process(ctx, list, processor); err != nil {
+		s.log.Error().Err(err).Str("type", string(list.Type)).Str("list", list.Name).Msg("error refreshing list")
+
+		// update last run for list and set errs and status
+		list.LastRefreshStatus = domain.ListRefreshStatusError
+		list.LastRefreshData = err.Error()
+		list.LastRefreshTime = time.Now()
+
+		if updateErr := s.repo.UpdateLastRefresh(ctx, list); updateErr != nil {
+			s.log.Error().Err(updateErr).Str("type", string(list.Type)).Str("list", list.Name).Msg("error updating last refresh for list")
+			return updateErr
+		}
+
+		return err
+	}
+
+	list.LastRefreshStatus = domain.ListRefreshStatusSuccess
+	//listItem.LastRefreshData = err.Error()
+	list.LastRefreshTime = time.Now()
+
+	if updateErr := s.repo.UpdateLastRefresh(ctx, list); updateErr != nil {
+		s.log.Error().Err(updateErr).Str("type", string(list.Type)).Str("list", list.Name).Msg("error updating last refresh for list")
+		return updateErr
+	}
+
+	s.log.Debug().Str("list", list.Name).Msg("successfully refreshed list")
+
+	return nil
+}
+
+func (s *Service) process(ctx context.Context, list *domain.List, processor Processor) error {
+	l := s.log.With().Str("type", string(list.Type)).Str("list", list.Name).Logger()
+
+	filterUpdate, err := processor.Process(ctx)
+	if err != nil {
+		return errors.Wrap(err, "could not make new request for URL")
+	}
+
+	if filterUpdate == nil {
+		return nil
+	}
+
+	for _, filter := range list.Filters {
+		l.Debug().Str("filter", filter.Name).Int("filter_id", filter.ID).Msg("updating filter")
+
+		filterUpdate.ID = filter.ID
+
+		if err := s.filterSvc.UpdatePartial(ctx, *filterUpdate); err != nil {
+			return errors.Wrapf(err, "error updating filter: %d", filter.ID)
+		}
+
+		l.Debug().Str("filter", filter.Name).Int("filter_id", filter.ID).Msg("successfully updated filter")
+	}
+
+	return nil
+}

commit 247e2794bc7aa94ca6e8896343627466a48d1d14

fix(lists): assert pointer client type when refreshing arr lists
getClientInstance asserted the cached download client as a value type
(radarr.Client) while the downloader cache stores pointers
(*radarr.Client). The assertion failed for every arr list type, so all
Lidarr, Radarr, Readarr, Sonarr, Sportarr and Whisparr list refreshes
returned "download client N (TYPE) has runtime type *radarr.Client".

Assert *T inside the helper and add a regression test that exercises
getProcessor for each arr list type against a stub downloader service.
Remove the unused action.Service.getClientInstance, which had the same
assertion and no callers.

Regression from #2650.

internal/action/service.go
@@ -11,7 +11,6 @@ import (
 	"github.com/autobrr/autobrr/internal/domain"
 	"github.com/autobrr/autobrr/internal/downloader"
 	"github.com/autobrr/autobrr/internal/events"
-	"github.com/autobrr/autobrr/pkg/errors"
 	"github.com/autobrr/autobrr/pkg/sharedhttp"
 
 	"github.com/rs/zerolog"
@@ -119,27 +118,4 @@ func (s *Service) DeleteByFilterID(ctx context.Context, filterID int) error {
 
 func (s *Service) ToggleEnabled(actionID int) error {
 	return s.repo.ToggleEnabled(actionID)
-}
-
-func (s *Service) getClientInstance[T any](ctx context.Context, clientID int32) (*T, error) {
-	instance, err := s.downloaderSvc.GetInstance(ctx, clientID)
-	if err != nil {
-		return nil, err
-	}
-
-	cfg := instance.Config()
-	if cfg == nil {
-		return nil, errors.New("client %d has no config", clientID)
-	}
-
-	if !cfg.Enabled {
-		return nil, errors.New("client %s %s not enabled", cfg.Type, cfg.Name)
-	}
-
-	client, err := instance.ClientAs[T]()
-	if err != nil {
-		return nil, err
-	}
-
-	return &client, nil
 }

internal/list/processor.go
@@ -40,12 +40,7 @@ func (s *Service) getClientInstance[T any](ctx context.Context, clientID int) (*
 		return nil, errors.Errorf("client %s %s not enabled", cfg.Type, cfg.Name)
 	}
 
-	client, err := instance.ClientAs[T]()
-	if err != nil {
-		return nil, err
-	}
-
-	return &client, nil
+	return instance.ClientAs[*T]()
 }
 
 func (s *Service) getProcessor(ctx context.Context, list *domain.List) (Processor, error) {

internal/list/processor_test.go
@@ -1,0 +1,105 @@
+// Copyright (c) 2021 - 2025, Ludvig Lundgren and the autobrr contributors.
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+package list
+
+import (
+	"context"
+	"testing"
+
+	"github.com/autobrr/autobrr/internal/domain"
+	"github.com/autobrr/autobrr/internal/downloader"
+	"github.com/autobrr/autobrr/pkg/arr/lidarr"
+	"github.com/autobrr/autobrr/pkg/arr/radarr"
+	"github.com/autobrr/autobrr/pkg/arr/readarr"
+	"github.com/autobrr/autobrr/pkg/arr/sonarr"
+	"github.com/autobrr/autobrr/pkg/arr/sportarr"
+	"github.com/autobrr/autobrr/pkg/arr/whisparr"
+
+	"github.com/rs/zerolog"
+	"github.com/stretchr/testify/assert"
+)
+
+type mockDownloaderService struct {
+	instance *downloader.Instance
+}
+
+func (m *mockDownloaderService) GetInstance(_ context.Context, _ int32) (*downloader.Instance, error) {
+	return m.instance, nil
+}
+
+func TestService_getProcessor(t *testing.T) {
+	type fields struct {
+		clientType domain.DownloaderType
+		client     any
+	}
+	type args struct {
+		list *domain.List
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr assert.ErrorAssertionFunc
+	}{
+		{
+			name:    "lidarr",
+			fields:  fields{clientType: domain.DownloaderTypeLidarr, client: &lidarr.Client{}},
+			args:    args{list: &domain.List{Type: domain.ListTypeLidarr, ClientID: 1}},
+			wantErr: assert.NoError,
+		},
+		{
+			name:    "radarr",
+			fields:  fields{clientType: domain.DownloaderTypeRadarr, client: &radarr.Client{}},
+			args:    args{list: &domain.List{Type: domain.ListTypeRadarr, ClientID: 1}},
+			wantErr: assert.NoError,
+		},
+		{
+			name:    "readarr",
+			fields:  fields{clientType: domain.DownloaderTypeReadarr, client: &readarr.Client{}},
+			args:    args{list: &domain.List{Type: domain.ListTypeReadarr, ClientID: 1}},
+			wantErr: assert.NoError,
+		},
+		{
+			name:    "sonarr",
+			fields:  fields{clientType: domain.DownloaderTypeSonarr, client: &sonarr.Client{}},
+			args:    args{list: &domain.List{Type: domain.ListTypeSonarr, ClientID: 1}},
+			wantErr: assert.NoError,
+		},
+		{
+			name:    "sportarr",
+			fields:  fields{clientType: domain.DownloaderTypeSportarr, client: &sportarr.Client{}},
+			args:    args{list: &domain.List{Type: domain.ListTypeSportarr, ClientID: 1}},
+			wantErr: assert.NoError,
+		},
+		{
+			name:    "whisparr",
+			fields:  fields{clientType: domain.DownloaderTypeWhisparr, client: &whisparr.Client{}},
+			args:    args{list: &domain.List{Type: domain.ListTypeWhisparr, ClientID: 1}},
+			wantErr: assert.NoError,
+		},
+		{
+			name:    "client type mismatch",
+			fields:  fields{clientType: domain.DownloaderTypeRadarr, client: &radarr.Client{}},
+			args:    args{list: &domain.List{Type: domain.ListTypeSonarr, ClientID: 1}},
+			wantErr: assert.Error,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cfg := &domain.Downloader{ID: 1, Type: tt.fields.clientType, Enabled: true}
+			s := &Service{
+				log:           zerolog.Nop(),
+				downloaderSvc: &mockDownloaderService{instance: downloader.NewInstance(cfg, tt.fields.client)},
+			}
+
+			got, err := s.getProcessor(t.Context(), tt.args.list)
+			if !tt.wantErr(t, err) {
+				return
+			}
+			if err == nil {
+				assert.NotNil(t, got)
+			}
+		})
+	}
+}